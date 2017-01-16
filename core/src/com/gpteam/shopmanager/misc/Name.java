834ecfbc04df92d3df3fcd48e4bf62a4c51ff7e0
masmix1 <4masmix1@gmail.com>
2017-01-16 21:07:16 +0100

    added Name class

---
+++ core/src/com/gpteam/shopmanager/misc/Name.java
@@ -0,0 +1,23 @@
+package com.gpteam.shopmanager.misc;
+
+/*
+ * Created by masmix on 16.01.2017.
+ */
+public class Name {
+    private String name;
+
+    public Name(String name) {
+        if (!(name.length() < 0) || !(name.length() > 25))
+            this.name = name;
+        else
+            throw new IllegalArgumentException("Name is either empty or too long.");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
