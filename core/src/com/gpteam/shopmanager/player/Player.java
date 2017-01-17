6c3996ec1d37e18e79fe63f7edc11b6b096b2dff
masmix1 <4masmix1@gmail.com>
2017-01-17 16:47:00 +0100

    moved variables to a helper class "Character"

--- core/src/com/gpteam/shopmanager/player/Player.java
+++ core/src/com/gpteam/shopmanager/player/Player.java
@@ -1,24 +1,14 @@
 package com.gpteam.shopmanager.player;
 
-import com.gpteam.shopmanager.Character.Age;
-import com.gpteam.shopmanager.Character.Gender;
-import com.gpteam.shopmanager.Character.Name;
-import com.gpteam.shopmanager.Character.Stats;
-
 /*
  * Created by masmix on 16.01.2017.
  */
 public class Player {
-    private Name name;
-    private Age age;
-    private Gender gender;
-    private Stats stats;
+    private Character character;
 
-    public Player(Name name, Age age, Gender gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.stats = stats;
+    public Player(Character character) {
+        this.character = character;
     }
+
 
 }
