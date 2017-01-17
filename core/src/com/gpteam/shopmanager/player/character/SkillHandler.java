b3112e12c04cc7134d04537bcd6f199bdd583ea0
masmix1 <4masmix1@gmail.com>
2017-01-18 00:45:53 +0100

    renamed Stats to SkillHandler, initial work on Skills,

---
+++ core/src/com/gpteam/shopmanager/player/character/SkillHandler.java
@@ -0,0 +1,17 @@
+package com.gpteam.shopmanager.player.character;
+
+/*
+ * Created by masmix on 16.01.2017.
+ */
+public class SkillHandler {
+
+    public SkillHandler() {
+
+    }
+
+    static class Skill {
+        String expertise = "Ekspertyza";
+        String negotiation = "Negocjacja";
+        String jobQuality = "Jakość pracy";
+    }
+}
--- core/src/com/gpteam/shopmanager/player/character/Stats.java
+++
@@ -1,7 +0,0 @@
-package com.gpteam.shopmanager.player.character;
-
-/*
- * Created by masmix on 16.01.2017.
- */
-public class Stats {
-}
