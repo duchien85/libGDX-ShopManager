3701025595b0bd38919d55281322c87035007c98
masmix1 <4masmix1@gmail.com>
2017-01-20 00:55:13 +0100

    Remove bad, duplicate code

--- core/src/com/gpteam/shopmanager/Skill/Skill.java
+++ core/src/com/gpteam/shopmanager/Skill/Skill.java
@@ -6,7 +6,4 @@ import com.gpteam.shopmanager.Text.Text;
  * Created by masmix on 16.01.2017.
  */
 public class Skill {
-    public String expertise = Text.expertise;
-    public String negotiation = Text.negotiation;
-    public String workQuality = Text.workQuality;
 }
