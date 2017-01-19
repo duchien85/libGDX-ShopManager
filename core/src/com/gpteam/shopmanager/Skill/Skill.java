dacada8f6a30872ff23c3bbbd53ed309e5dd7618
masmix1 <4masmix1@gmail.com>
2017-01-19 23:59:06 +0100

    Add expertise, negotiation, workQuality variables

--- core/src/com/gpteam/shopmanager/Skill/Skill.java
+++ core/src/com/gpteam/shopmanager/Skill/Skill.java
@@ -1,10 +1,12 @@
 package com.gpteam.shopmanager.Skill;
 
+import com.gpteam.shopmanager.Text.Text;
+
 /*
- * Created by masmix on 18.01.2017.
+ * Created by masmix on 16.01.2017.
  */
 public class Skill {
-    // TODO
-    // mortadelka
-    // jest bardzo smaczna
+    public String expertise = Text.expertise;
+    public String negotiation = Text.negotiation;
+    public String workQuality = Text.workQuality;
 }
