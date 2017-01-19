06425f13a5d2c52d166dc8c08b562359ef391484
masmix1 <4masmix1@gmail.com>
2017-01-19 23:55:07 +0100

    Add SkillHandler variables

--- core/src/com/gpteam/shopmanager/Skill/SkillHandler.java
+++ core/src/com/gpteam/shopmanager/Skill/SkillHandler.java
@@ -1,8 +1,12 @@
 package com.gpteam.shopmanager.Skill;
 
+import com.gpteam.shopmanager.Text.Text;
+
 /*
  * Created by masmix on 16.01.2017.
  */
 public class SkillHandler {
-
+    public String expertise = Text.expertise;
+    public String negotiation = Text.negotiation;
+    public String workQuality = Text.workQuality;
 }
