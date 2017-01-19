946d0d8a63ad2feffc79e7af291b606a6368213b
masmix1 <4masmix1@gmail.com>
2017-01-19 16:15:35 +0100

    NeedsHandler: added 3 methods (addNeed, addNeeds, removeNeed), constructor and "needs" ArrayList

--- core/src/com/gpteam/shopmanager/Npc_builder/Needs/NeedsHandler.java
+++ core/src/com/gpteam/shopmanager/Npc_builder/Needs/NeedsHandler.java
@@ -1,8 +1,32 @@
 package com.gpteam.shopmanager.Npc_builder.Needs;
 
+import java.util.ArrayList;
+
 /*
  * Created by masmix on 19.01.2017.
  */
 public class NeedsHandler {
+    private ArrayList<Needs> needs;
 
+    public NeedsHandler(ArrayList<Needs> needs) {
+        this.needs = needs;
+        needs = new ArrayList<Needs>();
+    }
+
+    public void addNeed(Needs need) {
+        if (needs.contains(need))
+            needs.add(need);
+        else
+            throw new IllegalArgumentException("Object already contains this need. Found: " + need);
+    }
+
+    public void addNeeds(Needs... need) {
+        for (int i = 0; i < need.length; i++)
+            needs.add(need[i]);
+    }
+
+    public void removeNeed(Needs needs) {
+        if (this.needs.contains(needs.toString()))
+            this.needs.remove(needs);
+    }
 }
