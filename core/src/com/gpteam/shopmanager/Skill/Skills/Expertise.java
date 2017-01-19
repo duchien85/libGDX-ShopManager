4e502582844ac82de05fd23c8ba3bc4035997214
masmix1 <4masmix1@gmail.com>
2017-01-20 00:54:41 +0100

    Add Expertise class with getName, getDescription methods

---
+++ core/src/com/gpteam/shopmanager/Skill/Skills/Expertise.java
@@ -0,0 +1,16 @@
+package com.gpteam.shopmanager.Skill.Skills;
+
+import com.gpteam.shopmanager.Text.Text;
+
+/*
+ * Created by masmix on 20.01.2017.
+ */
+public class Expertise {
+    public String getName() {
+        return Text.expertise;
+    }
+
+    public String getDescription() {
+        return Text.expertiseDescription;
+    }
+}
