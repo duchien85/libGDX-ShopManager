11c90f332f73e0e2c867251802f129c354cc77a8
masmix1 <4masmix1@gmail.com>
2017-01-20 02:04:41 +0100

    Modify setLanguage method made modular

--- core/src/com/gpteam/shopmanager/Text/TextHandler.java
+++ core/src/com/gpteam/shopmanager/Text/TextHandler.java
@@ -1,25 +1,23 @@
 package com.gpteam.shopmanager.Text;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import com.gpteam.shopmanager.Text.Libraries.*;
 /*
  * Created by masmix on 18.01.2017.
  */
 public final class TextHandler {
-    private HashMap<String, Object> languageMap;
+    private HashMap<String, Class> languageMap;
 
     public TextHandler() {
-        languageMap = new HashMap<String, Object>();
+        languageMap = new HashMap<String, Class>();
         languageMap.put("EN", EN.class);
         languageMap.put("PL", PL.class);
     }
 
-    public void setLanguage(String language) {
-        if (language.equals("PL"))
-            PL.setLanguage();
-
-        else if (language.equals("EN"))
-            EN.setLanguage();
+    public void setLanguage(String language) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        if (languageMap.containsKey(language))
+            languageMap.get(language).getDeclaredMethod("setLanguage").invoke(null);
 
         else
             throw new IllegalArgumentException("There's no such region. Found: " + language + ".");
