3ceabd40b81dd8bcd537fc15ee25f823334e0339
masmix1 <4masmix1@gmail.com>
2017-01-20 22:12:12 +0100

    Move 2 constraints variables to ExpertiseVariables

--- core/src/com/gpteam/shopmanager/Skill/Skills/Expertise.java
+++ core/src/com/gpteam/shopmanager/Skill/Skills/Expertise.java
@@ -1,6 +1,8 @@
 package com.gpteam.shopmanager.Skill.Skills;
 
+import com.gpteam.shopmanager.Skill.SkillLogic;
 import com.gpteam.shopmanager.Text.Text;
+import com.gpteam.shopmanager.Variables.ExpertiseVariables;
 
 /*
  * Created by masmix on 20.01.2017.
@@ -8,14 +10,11 @@ import com.gpteam.shopmanager.Text.Text;
 public class Expertise {
     private int skillLevel; // TODO zmienic na skillLevel
 
-    private static int minSkillLevel = 0;
-    private static int maxSkillLevel = 120;
-
     public void Expertise(int skillLevel) {
-        if (skillLevel > maxSkillLevel || skillLevel < minSkillLevel)
+        if (skillLevel > SkillLogic.maxSkillLevel || skillLevel < SkillLogic.minSkillLevel)
             throw new IllegalArgumentException("Wrong skill amount (0 - 120). Found: " + skillLevel + ".");
         else
-            this.skillLevel = skillLevel;
+            ExpertiseVariables.skillLevel = skillLevel;
     }
 
     public String getName() {
