5799139fc3fa91fff218f133e68e5cfc712c9302
masmix1 <4masmix1@gmail.com>
2017-01-20 22:10:02 +0100

    Add 2 constraints methods

--- core/src/com/gpteam/shopmanager/Skill/SkillLogic.java
+++ core/src/com/gpteam/shopmanager/Skill/SkillLogic.java
@@ -6,4 +6,6 @@ import com.gpteam.shopmanager.Text.Text;
  * Created by masmix on 16.01.2017.
  */
 public class SkillLogic {
+    public static int minSkillLevel = 0;
+    public static int maxSkillLevel = 120;
 }
