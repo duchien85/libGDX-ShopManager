44e9f681b60120e69215946a61195bb14a349397
masmix1 <4masmix1@gmail.com>
2017-01-20 02:41:30 +0100

    Add constraints, TODO, skillAmount variable (will be renamed to skillLevel)

--- core/src/com/gpteam/shopmanager/Skill/Skills/Expertise.java
+++ core/src/com/gpteam/shopmanager/Skill/Skills/Expertise.java
@@ -6,6 +6,19 @@ import com.gpteam.shopmanager.Text.Text;
  * Created by masmix on 20.01.2017.
  */
 public class Expertise {
+private int skillAmount // TODO zmienic na skillLevel
+
+private static int minSkillAmount = 0;
+private static int maxSkillAmount = 120;
+
+public void Expertise(int skillAmount) {
+if (skillAmount > maxSkillAmount || skillAmount < minSkillAmount)
+throw new IllegalArgumentException("Wrong skill amount (0 - 120). Found: " + skillAmount + ".");
+
+else
+this.skillAmount = skillAmount;
+}
+
     public String getName() {
         return Text.expertise;
     }
