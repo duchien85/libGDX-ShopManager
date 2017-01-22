6b52d40fff2d4c34c63b58ea7432f5f0fd027544
masmix1 <4masmix1@gmail.com>
2017-01-22 16:31:50 +0100

    Rename class "Person" to "AILogic"

---
+++ core/src/com/gpteam/shopmanager/AI/AILogic.java
@@ -0,0 +1,8 @@
+package com.gpteam.shopmanager.AI;
+
+/*
+ * Created by masmix on 18.01.2017.
+ */
+public final class AILogic {
+
+}
--- core/src/com/gpteam/shopmanager/Person/Person.java
+++
@@ -1,8 +0,0 @@
-package com.gpteam.shopmanager.Person;
-
-/*
- * Created by masmix on 18.01.2017.
- */
-public final class Person {
-
-}
