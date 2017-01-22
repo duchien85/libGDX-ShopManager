7caab0ca78ee9a4fafa4221cc7d6a1335e160615
masmix1 <4masmix1@gmail.com>
2017-01-22 03:18:14 +0100

    Add Array of SocietyClass objects, constructor

--- core/src/com/gpteam/shopmanager/Society/SocietyClassHandler.java
+++ core/src/com/gpteam/shopmanager/Society/SocietyClassHandler.java
@@ -4,5 +4,9 @@ package com.gpteam.shopmanager.Society;
  * Created by masmix on 18.01.2017.
  */
 public class SocietyClassHandler {
+    SocietyClass[] societyClass;
 
+    public SocietyClassHandler(SocietyClass[] societyClass) {
+        this.societyClass = societyClass;
+    }
 }
