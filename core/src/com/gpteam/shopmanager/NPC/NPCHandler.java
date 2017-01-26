49ceab8c7ba88c870494580bcd037c6d48dde50e
masmix1 <4masmix1@gmail.com>
2017-01-26 22:48:13 +0100

    Add NPCHandler variables

---
+++ core/src/com/gpteam/shopmanager/NPC/NPCHandler.java
@@ -0,0 +1,14 @@
+package com.gpteam.shopmanager.NPC;
+
+import com.gpteam.shopmanager.Character.Character;
+import com.gpteam.shopmanager.Money.MoneyHandler;
+import com.gpteam.shopmanager.Npc_builder.Needs.NeedsHandler;
+import com.gpteam.shopmanager.Society.SocietyClassHandler;
+
+/*
+ * Created by masmix on 26.01.2017.
+ */
+public class NPCHandler {
+    NeedsHandler needsHandler;
+    MoneyHandler moneyHandler;
+}
