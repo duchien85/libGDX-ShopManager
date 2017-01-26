09bf10966c16cea858c46feac8308ada2c8aca8f
masmix1 <4masmix1@gmail.com>
2017-01-26 22:47:22 +0100

    Add NPCCharacter class, extending Character class

---
+++ core/src/com/gpteam/shopmanager/Character/NPCCharacter.java
@@ -0,0 +1,26 @@
+package com.gpteam.shopmanager.Character;
+
+import com.gpteam.shopmanager.NPC.NPCHandler;
+import com.gpteam.shopmanager.Player.Sex;
+import com.gpteam.shopmanager.Society.SocietyClassHandler;
+
+/*
+ * Created by masmix on 26.01.2017.
+ */
+public class NPCCharacter extends Character {
+    NPCHandler npcHandler;
+    SocietyClassHandler societyClassHandler;
+
+
+    public NPCCharacter(String name, int age, Sex sex) {
+        super(name, age, sex);
+    }
+
+    public NPCCharacter(String name, int age) {
+        super(name, age);
+    }
+
+    public NPCCharacter(String name) {
+        super(name);
+    }
+}
