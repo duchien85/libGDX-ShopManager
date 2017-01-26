ce7d1aeb358b260465ed0cec4267f73181d626ee
masmix1 <4masmix1@gmail.com>
2017-01-26 22:47:38 +0100

    Add MoneyHandler logic

--- core/src/com/gpteam/shopmanager/Money/MoneyHandler.java
+++ core/src/com/gpteam/shopmanager/Money/MoneyHandler.java
@@ -4,6 +4,13 @@ package com.gpteam.shopmanager.Money;
  * Created by masmix on 23.12.2016.
  */
 public class MoneyHandler {
-    // nie kontrolowane przez gracza obiekty posiadajace pieniadze beda modyfikowane przez ta klase
-    // jak i pieniadze gracza !!!
+    private float money;
+
+    public void add(float amount) {
+        money += amount;
+    }
+
+    public void remove(float amount) {
+        money -= amount;
+    }
 }
