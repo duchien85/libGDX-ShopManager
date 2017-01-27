8452c7214c59249feb04f126c93091530b932ee6
masmix1 <4masmix1@gmail.com>
2017-01-27 12:21:39 +0100

    Add variables, constructor body

--- core/src/com/gpteam/shopmanager/NPC/NPC.java
+++ core/src/com/gpteam/shopmanager/NPC/NPC.java
@@ -1,12 +1,20 @@
 package com.gpteam.shopmanager.NPC;
 
+import com.gpteam.shopmanager.Money.MoneyHandler;
+import com.gpteam.shopmanager.Npc_builder.Needs.NeedsHandler;
+import com.gpteam.shopmanager.Society.SocietyClassHandler;
+
 /*
  * Created by masmix on 26.01.2017.
  */
 public class NPC {
-
+    private NeedsHandler needsHandler;
+    private MoneyHandler moneyHandler;
+    private SocietyClassHandler societyClassHandler;
 
     public NPC() {
-
+        needsHandler = new NeedsHandler();
+        moneyHandler = new MoneyHandler();
+        societyClassHandler = new SocietyClassHandler();
     }
 }
