2fefa976461a546dc6dc6cb8897e4ef59b494bf1
masmix1 <4masmix1@gmail.com>
2017-01-27 12:21:08 +0100

    Move variables and initialization to NPCBuilder

--- core/src/com/gpteam/shopmanager/NPC/NPCHandler.java
+++ core/src/com/gpteam/shopmanager/NPC/NPCHandler.java
@@ -1,26 +1,17 @@
 package com.gpteam.shopmanager.NPC;
 
-import com.gpteam.shopmanager.Character.Character;
-import com.gpteam.shopmanager.Money.MoneyHandler;
 import com.gpteam.shopmanager.NPCBuilder.NPCBuilder;
-import com.gpteam.shopmanager.Npc_builder.Needs.NeedsHandler;
-import com.gpteam.shopmanager.Society.SocietyClassHandler;
 
 /*
  * Created by masmix on 26.01.2017.
  */
 public class NPCHandler {
-    private NeedsHandler needsHandler;
-    private MoneyHandler moneyHandler;
-    private SocietyClassHandler societyClassHandler;
+
     private NPCBuilder npcBuilder;
     private NPC[] npcs;
     private int npcAmount;
 
     public NPCHandler(int npcAmount) {
-        needsHandler = new NeedsHandler();
-        moneyHandler = new MoneyHandler();
-        societyClassHandler = new SocietyClassHandler();
         npcBuilder = new NPCBuilder();
         npcs = new NPC[npcAmount];
         this.npcAmount = npcAmount;
