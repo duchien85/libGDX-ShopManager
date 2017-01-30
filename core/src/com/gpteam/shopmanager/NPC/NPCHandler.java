128a75254c033d876f5183cb6c7cc8293ad5a56b
masmix1 <4masmix1@gmail.com>
2017-01-31 00:35:23 +0100

    Modify NPCHandler - add variables, methods

--- core/src/com/gpteam/shopmanager/NPC/NPCHandler.java
+++ core/src/com/gpteam/shopmanager/NPC/NPCHandler.java
@@ -1,6 +1,10 @@
 package com.gpteam.shopmanager.NPC;
 
 import com.gpteam.shopmanager.NPCBuilder.NPCBuilder;
+import com.gpteam.shopmanager.Needs.NeedsHandler;
+import com.gpteam.shopmanager.Society.SocietyClassHandler;
+
+import java.util.ArrayList;
 
 /*
  * Created by masmix on 26.01.2017.
@@ -8,25 +12,41 @@ import com.gpteam.shopmanager.NPCBuilder.NPCBuilder;
 public class NPCHandler {
 
     private NPCBuilder npcBuilder;
-    private NPC[] npcs;
-    private int npcAmount;
+    private ArrayList<NPC> npcs;
+    private int npcQuantity;
+
+    private int maxNpcAmount = 10000;
+    private int minNpcAmount = 0;
     
     private NeedsHandler needsHandler;
     private SocietyClassHandler societyHandler;
 
-    public NPCHandler(int npcAmount) {
-        npcBuilder = new NPCBuilder();
-        npcs = new NPC[npcAmount];
-        this.npcAmount = npcAmount;
-    }
-    
-    public void initialize() {
-    	npcBuilder = new NPCBuilder();
-    	npcs = npcBuilder.newNPC(npcAmount);
+    public NPCHandler(int npcQuantity) {
+        npcBuilder = new NPCBuilder(npcQuantity);
+        npcs = npcBuilder.newNPCList(npcQuantity);
+        initialize(npcs, npcQuantity);
     }
 
+    public int currentNpcCount() {
+        return npcQuantity;
+    }
 
+    public void initialize(ArrayList<NPC> npcs, int npcQuantity) {
+        for (int i = 0; i < npcQuantity; i++) {
+            npcs.add(new NPC(true));
+        }
+    }
 
+    public void addNpcs(int npcAmount) {
+
+    }
+
+    public void addNpcs(int npcAmount, boolean allRandom) {
+        if (npcAmount > maxNpcAmount || npcAmount < minNpcAmount)
+            throw new IllegalArgumentException();
+        else
+            npcs.add(npcBuilder.newNPC(npcAmount, allRandom));
+    }
 
 //    public NPC[] generateRandomNPC() {
 //        // TODO figure out how to handle generating and storing npcs
