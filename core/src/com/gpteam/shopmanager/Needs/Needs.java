ce95ae8c69caccfd323c76769968cbe6ef40862a
masmix1 <4masmix1@gmail.com>
2017-01-31 00:52:11 +0100

    Remove unnecessary semicolon

--- core/src/com/gpteam/shopmanager/Needs/Needs.java
+++ core/src/com/gpteam/shopmanager/Needs/Needs.java
@@ -4,5 +4,5 @@ package com.gpteam.shopmanager.Needs;
  * Created by masmix on 18.01.2017.
  */
 public enum Needs {
-    FOOD, DRINK, ENTERTAINMENT, CLOTHING, HAPPINES;
+    FOOD, DRINK, ENTERTAINMENT, CLOTHING, HAPPINES
 }
