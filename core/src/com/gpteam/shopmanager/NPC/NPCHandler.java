b1dcbbda5c283949f08e7084637d8203c1e3464c
masmix1 <4masmix1@gmail.com>
2017-02-01 12:09:46 +0100

    Add constraints in constructor, Remove initialize method, Rename argument

--- core/src/com/gpteam/shopmanager/NPC/NPCHandler.java
+++ core/src/com/gpteam/shopmanager/NPC/NPCHandler.java
@@ -10,35 +10,32 @@ import java.util.ArrayList;
  * Created by masmix on 26.01.2017.
  */
 public class NPCHandler {
-
     private NPCBuilder npcBuilder;
     private ArrayList<NPC> npcs;
     private int npcQuantity = 0;
-
-    public static final int MAX_NPC_AMOUNT = 10000;
-    public static final int MIN_NPC_AMOUNT = 0;
-    
     private NeedsHandler needsHandler;
     private SocietyClassHandler societyHandler;
 
+    public static final int MAX_NPC_AMOUNT = 10000;
+    public static final int MIN_NPC_AMOUNT = 0;
+
+
     public NPCHandler(int npcQuantity) {
-        npcBuilder = new NPCBuilder();
-        npcs = npcBuilder.newNPCList(npcQuantity);
-        this.npcQuantity += npcQuantity;
+        if (npcQuantity > MAX_NPC_AMOUNT || npcQuantity < MIN_NPC_AMOUNT)
+            throw new IllegalArgumentException("npcQuantity must be between 0 - 10000. Found: " + npcQuantity);
+        else {
+            npcBuilder = new NPCBuilder();
+            npcs = npcBuilder.newNPCList(npcQuantity);
+            this.npcQuantity += npcQuantity;
+        }
     }
     
     public NPCHandler() {
-    npcs = new ArrayList<NPC>();
+        npcs = new ArrayList<NPC>();
     }
 
     public int currentNpcCount() {
         return npcQuantity;
-    }
-
-    public void initialize(ArrayList<NPC> npcs, int npcQuantity) {
-        for (int i = 0; i < npcQuantity; i++) {
-            npcs.add(npcBuilder.newNPC());
-        }
     }
 
     public void addNpcs(int npcQuantity) {
@@ -51,12 +48,15 @@ public class NPCHandler {
         this.npcQuantity += npcQuantity;
     }
 
-    public void addNpcs(int npcAmount, boolean allRandom) {
-        if (npcAmount > MAX_NPC_AMOUNT || npcAmount < MIN_NPC_AMOUNT)
-            throw new IllegalArgumentException();
+    public void addNpcs(int npcQuantity, boolean allRandom) {
+        if (npcQuantity > MAX_NPC_AMOUNT || npcQuantity < MIN_NPC_AMOUNT)
+            throw new IllegalArgumentException("npcQuantity must be between 0 - 10000. Found: " + npcQuantity);
         else
-            for (int i = 0; i < npcAmount; i++)
+            for (int i = 0; i < npcQuantity; i++)
                 npcs.add(npcBuilder.newNPC(allRandom));
+
+        this.npcQuantity += npcQuantity;
+
     }
     
     public void removeNpc(NPC npc) {
