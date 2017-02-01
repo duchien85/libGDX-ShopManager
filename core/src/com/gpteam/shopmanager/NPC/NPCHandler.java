36f6389b2c99dc8a0cd86c7713a4edc6f29cc076
masmix1 <4masmix1@gmail.com>
2017-02-01 11:36:19 +0100

    Add code for npcQuantity variable

--- core/src/com/gpteam/shopmanager/NPC/NPCHandler.java
+++ core/src/com/gpteam/shopmanager/NPC/NPCHandler.java
@@ -13,7 +13,7 @@ public class NPCHandler {
 
     private NPCBuilder npcBuilder;
     private ArrayList<NPC> npcs;
-    private int npcQuantity;
+    private int npcQuantity = 0;
 
     public static final int MAX_NPC_AMOUNT = 10000;
     public static final int MIN_NPC_AMOUNT = 0;
@@ -24,10 +24,11 @@ public class NPCHandler {
     public NPCHandler(int npcQuantity) {
         npcBuilder = new NPCBuilder();
         npcs = npcBuilder.newNPCList(npcQuantity);
+        this.npcQuantity += npcQuantity;
     }
     
     public NPCHandler() {
-    npcs = new ArrayList<>();
+    npcs = new ArrayList<NPC>();
     }
 
     public int currentNpcCount() {
@@ -40,12 +41,14 @@ public class NPCHandler {
         }
     }
 
-    public void addNpcs(int npcAmount) {
-    if (npcAmount > MAX_NPC_AMOUNT || npcAmount < MIN_NPC_AMOUNT)
+    public void addNpcs(int npcQuantity) {
+    if (npcQuantity > MAX_NPC_AMOUNT || npcQuantity < MIN_NPC_AMOUNT)
             throw new IllegalArgumentException();
         else
-            for (int i = 0; i < npcAmount; i++)
+            for (int i = 0; i < npcQuantity; i++)
                 npcs.add(npcBuilder.newNPC());
+
+        this.npcQuantity += npcQuantity;
     }
 
     public void addNpcs(int npcAmount, boolean allRandom) {
@@ -57,14 +60,13 @@ public class NPCHandler {
     }
     
     public void removeNpc(NPC npc) {
-    if (npcs.contains(npc)
-    npcs.remove(npc)
-    else
-    throw new IllegalArgumentException();
+        if (npcs.contains(npc))
+            npcs.remove(npc);
+        else
+            throw new IllegalArgumentException();
     }
 
 //    public NPC[] generateRandomNPC() {
 //        // TODO figure out how to handle generating and storing npcs
 //    }
-
 }
