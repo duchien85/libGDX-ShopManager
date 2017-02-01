fe2b5bde31ec166e72f585a6b80e7c18d29db7f8
masmix1 <4masmix1@gmail.com>
2017-02-01 13:45:13 +0100

    Add variable, constructor

--- core/src/com/gpteam/shopmanager/Society/SocietyClassHandler.java
+++ core/src/com/gpteam/shopmanager/Society/SocietyClassHandler.java
@@ -1,12 +1,27 @@
 package com.gpteam.shopmanager.Society;
 
+import com.badlogic.gdx.math.MathUtils;
+import com.gpteam.shopmanager.NPC.NPC;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /*
  * Created by masmix on 18.01.2017.
  */
 public class SocietyClassHandler {
-    SocietyClass[] societyClass;
+    ArrayList<SocietyClass> societyClass;
+    SocietyClass[] societyClasses;
 
     public SocietyClassHandler(SocietyClass... societyClass) {
-        this.societyClass = societyClass;
+        societyClasses = new SocietyClass[SocietyClass.values().length];
+        this.societyClass = new ArrayList<SocietyClass>();
+        this.societyClass.addAll(Arrays.asList(societyClass));
+    }
+
+    public SocietyClassHandler(boolean allRandom) {
+        societyClasses = new SocietyClass[SocietyClass.values().length];
+        societyClass = new ArrayList<SocietyClass>();
+        societyClass.add(societyClasses[MathUtils.random(SocietyClass.values().length)]);
     }
 }
