bf302e9e666225c69baa1b00072a70fa4c5e846f
masmix1 <4masmix1@gmail.com>
2017-02-01 20:21:43 +0100

    Add skillLevel variable

--- core/src/com/gpteam/shopmanager/Skill/Skills/Expertise.java
+++ core/src/com/gpteam/shopmanager/Skill/Skills/Expertise.java
@@ -8,10 +8,10 @@ import com.gpteam.shopmanager.Variables.ExpertiseVariables;
  * Created by masmix on 20.01.2017.
  */
 public class Expertise {
-    private int skillLevel;
+    private int skillLevel = 120;
 
-    public void Expertise(int skillLevel) {
-        if (skillLevel > SkillLogic.maxSkillLevel || skillLevel < SkillLogic.minSkillLevel)
+    public Expertise(int skillLevel) {
+        if (skillLevel > this.skillLevel || skillLevel < this.skillLevel)
             throw new IllegalArgumentException("Wrong skill amount (0 - 120). Found: " + skillLevel + ".");
         else
             ExpertiseVariables.skillLevel = skillLevel;
