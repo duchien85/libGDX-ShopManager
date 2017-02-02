3e0b589b3161f8f8380b51246063d54357fdc1e1
masmix1 <4masmix1@gmail.com>
2017-02-02 20:27:20 +0100

    Add initializeHashMap method, Replace Needs Aray with HashMap - Needs, Integer

--- core/src/com/gpteam/shopmanager/Needs/NeedsHandler.java
+++ core/src/com/gpteam/shopmanager/Needs/NeedsHandler.java
@@ -1,22 +1,32 @@
 package com.gpteam.shopmanager.Needs;
 
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.NoSuchElementException;
 
 /*
  * Created by masmix on 19.01.2017.
  */
 public class NeedsHandler {
-    private Needs[] needs;
+    private HashMap<Needs, Integer> needs;
     private static int needsLength = Needs.values().length;
 
-
     public NeedsHandler() {
-        needs = Needs.values();
+        initializeHashMap(needs, Needs.values().length);
     }
     
     public void check() {
+    }
+
+    private void initializeHashMap(HashMap<Needs, Integer> hashMap, int initialCapacity) {
+        hashMap = new HashMap<Needs, Integer>(initialCapacity);
+        for (HashMap.Entry<Needs, Integer> entry : hashMap.entrySet()) {
+            for (Needs x : Needs.values())
+                hashMap.put(x, null);
+        }
     }
 }
