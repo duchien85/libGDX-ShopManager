dfb426c048ac3fe7b85d60f711af13ddb03db74c
masmix1 <4masmix1@gmail.com>
2017-02-02 20:36:24 +0100

    Add todo

--- core/src/com/gpteam/shopmanager/Needs/NeedsLogic.java
+++ core/src/com/gpteam/shopmanager/Needs/NeedsLogic.java
@@ -4,4 +4,5 @@ package com.gpteam.shopmanager.Needs;
  * Created by masmix on 02.02.2017.
  */
 public class NeedsLogic {
+    // TODO handling of Needs growth and decrease - multiplied by some scaling factor / algorythm?
 }
