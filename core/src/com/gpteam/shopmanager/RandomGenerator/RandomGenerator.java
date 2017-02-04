de66caf8b6ba39a9e6d8083de9108e0e12acdd80
masmix1 <4masmix1@gmail.com>
2017-02-04 20:37:08 +0100

    Add crucial methods

--- core/src/com/gpteam/shopmanager/RandomGenerator/RandomGenerator.java
+++ core/src/com/gpteam/shopmanager/RandomGenerator/RandomGenerator.java
@@ -1,20 +1,44 @@
 package com.gpteam.shopmanager.RandomGenerator;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.gpteam.shopmanager.Character.CharacterInfo;
-import com.gpteam.shopmanager.Needs.NeedsHandler;
+import com.gpteam.shopmanager.IOHandlers.OutputHandlers.NamesLoader;
 import com.gpteam.shopmanager.Player.Sex;
-import com.gpteam.shopmanager.Society.SocietyClassHandler;
+import com.gpteam.shopmanager.Variables.Variables;
 
 /*
  * Created by masmix on 04.02.2017.
  */
 public class RandomGenerator {
+    private NamesLoader namesLoader;
 
-    public static CharacterInfo generateRandomInfo() {
+    public RandomGenerator(NamesLoader namesLoader) {
+        this.namesLoader = namesLoader;
+    }
+
+    public CharacterInfo generateRandomInfo() {
+        return new CharacterInfo(getRandomName(), getRandomAge(), getRandomSex());
+    }
+
+    private String getRandomName() {
         String name;
-        int age;
-        Sex sex;
+        do { name = namesLoader.getNames().get(MathUtils.random(0, namesLoader.getNames().size() - 1));
+        } while (validateName(name));
+        return name;
+    }
 
+    private int getRandomAge() {
+        return MathUtils.random(Variables.MIN_AGE_LENGTH, Variables.MAX_AGE_LENGTH);
+    }
 
+    private Sex getRandomSex() {
+        return Sex.getRandom();
+    }
+
+    private boolean validateName(String name) {
+        if (name.length() > Variables.MAX_NAME_LENGTH || name.length() < Variables.MIN_NAME_LENGTH)
+            return false;
+        else
+            return true;
     }
 }
