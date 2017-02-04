a65ed17472a2e8d92a5e165e9d89580265cdcb52
masmix1 <4masmix1@gmail.com>
2017-02-04 21:49:38 +0100

    Add constructor body, getters, setters

--- core/src/com/gpteam/shopmanager/Character/CharacterInfo.java
+++ core/src/com/gpteam/shopmanager/Character/CharacterInfo.java
@@ -1,6 +1,7 @@
 package com.gpteam.shopmanager.Character;
 
 import com.gpteam.shopmanager.Player.Sex;
+import com.gpteam.shopmanager.RandomGenerator.RandomGenerator;
 
 /*
  * Created by masmix on 04.02.2017.
@@ -9,6 +10,7 @@ public class CharacterInfo {
     private String name;
     private int age;
     private Sex sex;
+    private RandomGenerator randomGenerator;
 
     public CharacterInfo(String name, int age, Sex sex) {
         this.name = name;
@@ -17,6 +19,33 @@ public class CharacterInfo {
     }
 
     public CharacterInfo(boolean allRandom) {
+        if (allRandom) {
+            randomGenerator = new RandomGenerator();
+            randomGenerator.generateRandomInfo();
+        }
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
     }
 }
