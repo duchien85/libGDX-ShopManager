44639e2c613505e55b774c442e5abc8f9aa179de
masmix1 <4masmix1@gmail.com>
2017-02-04 20:36:06 +0100

    Add Variables class

---
+++ core/src/com/gpteam/shopmanager/Variables/Variables.java
@@ -0,0 +1,13 @@
+package com.gpteam.shopmanager.Variables;
+
+/*
+ * Created by masmix on 04.02.2017.
+ */
+public final class Variables {
+    // Character
+    public final static int MAX_NAME_LENGTH = 25;
+    public final static int MIN_NAME_LENGTH = 2;
+
+    public final static int MAX_AGE_LENGTH = 120;
+    public final static int MIN_AGE_LENGTH = 16;
+}
