8a461d8d9266f4621d43323a47b62e8e5762f254
masmix1 <4masmix1@gmail.com>
2017-02-04 18:11:13 +0100

    Add RandomGenerator class

---
+++ core/src/com/gpteam/shopmanager/RandomGenerator/RandomGenerator.java
@@ -0,0 +1,20 @@
+package com.gpteam.shopmanager.RandomGenerator;
+
+import com.gpteam.shopmanager.Character.CharacterInfo;
+import com.gpteam.shopmanager.Needs.NeedsHandler;
+import com.gpteam.shopmanager.Player.Sex;
+import com.gpteam.shopmanager.Society.SocietyClassHandler;
+
+/*
+ * Created by masmix on 04.02.2017.
+ */
+public class RandomGenerator {
+
+    public static CharacterInfo generateRandomInfo() {
+        String name;
+        int age;
+        Sex sex;
+
+
+    }
+}
