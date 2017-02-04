c83c82890593c4be8c44c46ab93739ec2a735fb3
masmix1 <4masmix1@gmail.com>
2017-02-04 21:45:37 +0100

    Add "getSkillLevel" method, Modify constructor to use Variables class

--- core/src/com/gpteam/shopmanager/Skill/Skills/Expertise.java
+++ core/src/com/gpteam/shopmanager/Skill/Skills/Expertise.java
@@ -2,19 +2,23 @@ package com.gpteam.shopmanager.Skill.Skills;
 
 import com.gpteam.shopmanager.Skill.SkillLogic;
 import com.gpteam.shopmanager.Text.Text;
-import com.gpteam.shopmanager.Variables.ExpertiseVariables;
+import com.gpteam.shopmanager.Variables.Variables;
 
 /*
  * Created by masmix on 20.01.2017.
  */
 public class Expertise {
-    private int skillLevel = 120;
+    private int skillLevel;
 
     public Expertise(int skillLevel) {
-        if (skillLevel > this.skillLevel || skillLevel < this.skillLevel)
+        if (skillLevel > Variables.MAX_SKILL_LEVEL || skillLevel < Variables.MIN_SKILL_LEVEL)
             throw new IllegalArgumentException("Wrong skill amount (0 - 120). Found: " + skillLevel + ".");
         else
-            ExpertiseVariables.skillLevel = skillLevel;
+            this.skillLevel = skillLevel;
+    }
+
+    public int getSkillLevel() {
+        return skillLevel;
     }
 
     public String getName() {
