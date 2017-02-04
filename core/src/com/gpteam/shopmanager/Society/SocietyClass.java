bf20fd3b9ecc50d5384418ca22a0df7ab3a07f35
masmix1 <4masmix1@gmail.com>
2017-02-04 19:32:41 +0100

    Add "getRandom" method

--- core/src/com/gpteam/shopmanager/Society/SocietyClass.java
+++ core/src/com/gpteam/shopmanager/Society/SocietyClass.java
@@ -4,5 +4,9 @@ package com.gpteam.shopmanager.Society;
  * Created by masmix on 18.01.2017.
  */
 public enum SocietyClass {
-    RICH, POOR, DRUNKS, MIDDLE_CLASS, KIDS, STUDENTS
+    RICH, POOR, DRUNKS, MIDDLE_CLASS, KIDS, STUDENTS;
+
+    public static SocietyClass getRandom() {
+        return values()[(int) (Math.random() * values().length)];
+    }
 }
