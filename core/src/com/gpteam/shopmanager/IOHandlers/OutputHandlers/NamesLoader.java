8c4036d404ce5a723186159cc6046e76477b1555
masmix1 <4masmix1@gmail.com>
2017-02-04 20:35:51 +0100

    Add constructor, "load", "getNames" methods

---
+++ core/src/com/gpteam/shopmanager/IOHandlers/OutputHandlers/NamesLoader.java
@@ -0,0 +1,30 @@
+package com.gpteam.shopmanager.IOHandlers.OutputHandlers;
+
+import com.gpteam.shopmanager.IOHandlers.Loader;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/*
+ * Created by masmix on 02.02.2017.
+ */
+public class NamesLoader {
+    private ArrayList<String> names;
+    private Loader loader;
+
+    public NamesLoader(Loader loader) {
+        this.loader = loader;
+        names = new ArrayList<String>();
+    }
+
+    public void load() {
+        String line;
+        try { while ((line = loader.getBufferedReader().readLine()) != null)
+            names.add(line);
+        } catch (IOException e) { e.printStackTrace(); }
+    }
+
+    public ArrayList<String> getNames() {
+        return names;
+    }
+}
--- core/src/com/gpteam/shopmanager/Loader/Loader.java
+++
@@ -1,7 +0,0 @@
-package com.gpteam.shopmanager.Loader;
-
-/*
- * Created by masmix on 02.02.2017.
- */
-public class Loader {
-}
