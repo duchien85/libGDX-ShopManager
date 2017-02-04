14f9d698d94e5fd3da2b4fd5e748bc42a2adf06a
masmix1 <4masmix1@gmail.com>
2017-02-04 14:27:21 +0100

    Rename class "NPCCharacter" to "NPCInfo"

--- core/src/com/gpteam/shopmanager/Character/NPCInfo.java
+++ core/src/com/gpteam/shopmanager/Character/NPCInfo.java
@@ -7,17 +7,17 @@ import com.gpteam.shopmanager.Player.Sex;
 /*
  * Created by masmix on 26.01.2017.
  */
-public class NPCCharacter extends Character {
+public class NPCInfo extends Character {
 
-    public NPCCharacter(String name, int age, Sex sex) {
+    public NPCInfo(String name, int age, Sex sex) {
         super(name, age, sex);
     }
 
-    public NPCCharacter(String name, int age) {
+    public NPCInfo(String name, int age) {
         super(name, age);
     }
 
-    public NPCCharacter(String name) {
+    public NPCInfo(String name) {
         super(name);
     }
 }
