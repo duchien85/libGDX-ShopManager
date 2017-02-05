5a6e520d38c86205e863ccc92df3a93bd3104bff
masmix1 <4masmix1@gmail.com>
2017-02-05 20:16:58 +0100

    Remove redundant variables

--- core/src/com/gpteam/shopmanager/Skill/SkillLogic.java
+++ core/src/com/gpteam/shopmanager/Skill/SkillLogic.java
@@ -6,6 +6,4 @@ import com.gpteam.shopmanager.Text.Text;
  * Created by masmix on 16.01.2017.
  */
 public class SkillLogic {
-    public static int minSkillLevel = 0;
-    public static int maxSkillLevel = 120;
 }
