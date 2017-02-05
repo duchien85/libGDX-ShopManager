50d5d4285b1c9d9eb9a287b1ff4f782d6575d23d
masmix1 <4masmix1@gmail.com>
2017-02-05 21:05:41 +0100

    Modify object, import

--- core/src/com/gpteam/shopmanager/Player/Player.java
+++ core/src/com/gpteam/shopmanager/Player/Player.java
@@ -1,14 +1,15 @@
 package com.gpteam.shopmanager.Player;
 
-import com.gpteam.shopmanager.Character.Character;
+import com.gpteam.shopmanager.Character.CharacterInfo;
+import com.gpteam.shopmanager.Deprecated.Character;
 
 /*
  * Created by masmix on 16.01.2017.
  */
 public class Player {
-    public Character character;
+    public CharacterInfo characterInfo;
 
-    public Player(Character character) {
-        this.character = character;
+    public Player(CharacterInfo characterInfo) {
+        this.characterInfo = characterInfo;
     }
 }
