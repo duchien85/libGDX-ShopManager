232728823f443aa2f1350e53a3801692ca1dc430
masmix1 <4masmix1@gmail.com>
2017-02-05 20:16:46 +0100

    Add javadocs linking to corresponding variables

--- core/src/com/gpteam/shopmanager/Variables/Variables.java
+++ core/src/com/gpteam/shopmanager/Variables/Variables.java
@@ -1,25 +1,38 @@
 package com.gpteam.shopmanager.Variables;
 
+import com.gpteam.shopmanager.FileHandlers.Loaders.NamesLoader;
+import com.gpteam.shopmanager.NPC.NPCHandler;
+import com.gpteam.shopmanager.Skill.SkillLogic;
+import com.gpteam.shopmanager.Character.Character;
+
 /*
  * Created by masmix on 04.02.2017.
  */
 public final class Variables {
-    // "names.txt" default location - NamesLoader
+    /**************************************************
+     * {@link NamesLoader}
+     * NAMES_PATH is the location
+     * of txt file with random names
+     **************************************************/
     public final static String NAMES_PATH = "names.txt";
 
-    // =================================================
-
-    // Character
+    /**************************************************
+     * {@link Character}
+     **************************************************/
     public final static int MAX_NAME_LENGTH = 25;
     public final static int MIN_NAME_LENGTH = 2;
     public final static int MAX_AGE_LENGTH = 120;
     public final static int MIN_AGE_LENGTH = 16;
 
-    // Skills
+    /**************************************************
+     * {@link SkillLogic}
+     **************************************************/
     public final static int MAX_SKILL_LEVEL = 120;
     public final static int MIN_SKILL_LEVEL = 0;
 
-    // NPCHandler
+    /**************************************************
+     * {@link NPCHandler}
+     **************************************************/
     public final static int MAX_NPC_AMOUNT = 10000;
     public final static int MIN_NPC_AMOUNT = 0;
 }
