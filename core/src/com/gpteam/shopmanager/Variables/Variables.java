64ce5661ef9db97f8a69027f734c12da1f0ac685
masmix1 <4masmix1@gmail.com>
2017-02-05 15:19:53 +0100

    Add "NAMES_PATH" variable

--- core/src/com/gpteam/shopmanager/Variables/Variables.java
+++ core/src/com/gpteam/shopmanager/Variables/Variables.java
@@ -4,14 +4,22 @@ package com.gpteam.shopmanager.Variables;
  * Created by masmix on 04.02.2017.
  */
 public final class Variables {
+    // "names.txt" default location - NamesLoader
+    public final static String NAMES_PATH = "names.txt";
+
+    // =================================================
+
     // Character
     public final static int MAX_NAME_LENGTH = 25;
     public final static int MIN_NAME_LENGTH = 2;
-
     public final static int MAX_AGE_LENGTH = 120;
     public final static int MIN_AGE_LENGTH = 16;
 
     // Skills
     public final static int MAX_SKILL_LEVEL = 120;
     public final static int MIN_SKILL_LEVEL = 0;
+
+    // NPCHandler
+    public final static int MAX_NPC_AMOUNT = 10000;
+    public final static int MIN_NPC_AMOUNT = 0;
 }
