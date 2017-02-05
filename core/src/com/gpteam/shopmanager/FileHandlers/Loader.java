6f4addc2e50bbb4df86ca8bbb1ee3ba11bc7dc50
masmix1 <4masmix1@gmail.com>
2017-02-05 15:24:29 +0100

    Import static variable "NAMES_PATH"

--- core/src/com/gpteam/shopmanager/FileHandlers/Loader.java
+++ core/src/com/gpteam/shopmanager/FileHandlers/Loader.java
@@ -1,9 +1,11 @@
-package com.gpteam.shopmanager.IOHandlers;
+package com.gpteam.shopmanager.FileHandlers;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+
+import static com.gpteam.shopmanager.Variables.Variables.NAMES_PATH;
 
 /*
  * Created by masmix on 04.02.2017.
@@ -15,6 +17,12 @@ public final class Loader {
 
     public Loader(String fileName) {
         file = new File(fileName);
+        try { fileReader = new FileReader(file); } catch (FileNotFoundException e) { e.printStackTrace(); }
+        bufferedReader = new BufferedReader(fileReader);
+    }
+
+    public Loader() {
+        file = new File(NAMES_PATH);
         try { fileReader = new FileReader(file); } catch (FileNotFoundException e) { e.printStackTrace(); }
         bufferedReader = new BufferedReader(fileReader);
     }
