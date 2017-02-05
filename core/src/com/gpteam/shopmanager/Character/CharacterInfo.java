96ead5a46c891ce0a3cc8c0dd2f88d35c76c05b7
masmix1 <4masmix1@gmail.com>
2017-02-05 21:01:00 +0100

    Add assign, validateAndAssign, validate methods

--- core/src/com/gpteam/shopmanager/Character/CharacterInfo.java
+++ core/src/com/gpteam/shopmanager/Character/CharacterInfo.java
@@ -3,6 +3,8 @@ package com.gpteam.shopmanager.Character;
 import com.gpteam.shopmanager.Player.Sex;
 import com.gpteam.shopmanager.RandomGenerator.RandomGenerator;
 
+import static com.gpteam.shopmanager.Variables.Variables.*;
+
 /*
  * Created by masmix on 04.02.2017.
  */
@@ -12,9 +14,7 @@ public class CharacterInfo {
     private Sex sex;
 
     public CharacterInfo(String name, int age, Sex sex) {
-        this.name = name;
-        this.age = age;
-        this.sex = sex;
+        validateAndAssign(name, age, sex);
     }
 
     public CharacterInfo() {
@@ -50,5 +50,49 @@ public class CharacterInfo {
         name = randomGenerator.getRandomName();
         age = randomGenerator.getRandomAge();
         sex = randomGenerator.getRandomSex();
+    }
+
+    private void assign(String name, int age, Sex sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    private void validateAndAssign(String name, int age, Sex sex) {
+        if (validate(name, age, sex))
+            assign(name, age, sex);
+        else
+            throw new IllegalArgumentException("One of the arguments was invalid. Found: name: " + name + ", age: " + age + ", sex: " + sex.toString());
+    }
+
+    private boolean validate(String name, int age, Sex sex) {
+        if (validate(name) && validate(age) && validate(sex))
+            return true;
+        else
+            return false;
+    }
+
+    private boolean validate(String name) {
+        if (name.length() > MAX_NAME_LENGTH || name.length() < MIN_NAME_LENGTH)
+            throw new IllegalArgumentException("Name must contain at least 2 characters, up to 25. Found: " + name.length());
+        else
+            return true;
+    }
+
+    private boolean validate(int age) {
+        if (age > MAX_AGE_LENGTH || age < MIN_AGE_LENGTH)
+            throw new IllegalArgumentException("Age must be at least 12 and no higher than 120. Found: " + age);
+        else
+            return true;
+    }
+
+    private boolean validate(Sex sex) {
+        if (sex.equals(Sex.MALE))
+            return true;
+
+        else if (sex.equals(Sex.FEMALE))
+            return true;
+
+        else throw new IllegalArgumentException("Sex must be either male or female. Found: " + sex.toString());
     }
 }
