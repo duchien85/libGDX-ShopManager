0d42c564bad43243235df0627b1456bfb89fa33d
masmix1 <4masmix1@gmail.com>
2017-02-07 02:00:28 +0100

    Rename enum to make it follow a standard

--- core/src/com/gpteam/shopmanager/Society/SocietyClass.java
+++ core/src/com/gpteam/shopmanager/Society/SocietyClass.java
@@ -4,7 +4,7 @@ package com.gpteam.shopmanager.Society;
  * Created by masmix on 18.01.2017.
  */
 public enum SocietyClass {
-    RICH, POOR, DRUNK, MIDDLE_CLASS, TEENAGER, STUDENTS;
+    RICH, POOR, DRUNK, MIDDLE_CLASS, TEENAGER, STUDENT;
 
     public static SocietyClass getRandom() {
         return values()[(int) (Math.random() * values().length)];
