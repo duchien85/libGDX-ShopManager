3eaa9b530ea71c338bbcd5a8af708bb6b39f4a67
masmix1 <4masmix1@gmail.com>
2017-02-11 21:14:32 +0100

    Add "handleIllegalArgumentException" method

--- core/src/com/gpteam/shopmanager/Engine/Modules/ErrorHandler.java
+++ core/src/com/gpteam/shopmanager/Engine/Modules/ErrorHandler.java
@@ -4,4 +4,7 @@ package com.gpteam.shopmanager.Engine.Modules;
  * Created by masmix on 11.02.2017.
  */
 public class ErrorHandler {
+    public void handleIllegalArgumentException(String message) {
+        throw new IllegalArgumentException(message);
+    }
 }
