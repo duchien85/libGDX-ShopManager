eaa3ef0b3c5a91ab4d9e5f939d6bf6779d269502
masmix1 <4masmix1@gmail.com>
2017-02-11 21:24:58 +0100

    Make "ErrorHandler" class static

--- core/src/com/gpteam/shopmanager/Engine/Modules/ErrorHandler.java
+++ core/src/com/gpteam/shopmanager/Engine/Modules/ErrorHandler.java
@@ -3,7 +3,7 @@ package com.gpteam.shopmanager.Engine.Modules;
 /*
  * Created by masmix on 11.02.2017.
  */
-public class ErrorHandler {
+public final class ErrorHandler {
     public static void handleIllegalArgumentException(String message) {
         throw new IllegalArgumentException(message);
     }
