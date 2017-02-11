929f6bac73b59947ad348af41c50a8ae24a02a40
masmix1 <4masmix1@gmail.com>
2017-02-11 20:48:50 +0100

    Remove unused imports

--- core/src/com/gpteam/shopmanager/Needs/NeedsHandler.java
+++ core/src/com/gpteam/shopmanager/Needs/NeedsHandler.java
@@ -1,13 +1,6 @@
 package com.gpteam.shopmanager.Needs;
 
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Vector2;
-
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.NoSuchElementException;
 
 /*
  * Created by masmix on 19.01.2017.
