fda5e4ea749b37b2198ee900baf5ba4469d03c7b
masmix1 <4masmix1@gmail.com>
2017-02-13 03:51:23 +0100

    Add javadocs with links to classes

--- core/src/com/gpteam/shopmanager/Text/Text.java
+++ core/src/com/gpteam/shopmanager/Text/Text.java
@@ -1,5 +1,8 @@
 package com.gpteam.shopmanager.Text;
 
+import com.gpteam.shopmanager.Traffic.TrafficHandler;
+import com.gpteam.shopmanager.Society.SocietyClass;
+import com.gpteam.shopmanager.Character.CharacterInfo;
 /*
  * Created by masmix on 18.01.2017.
  */
@@ -10,13 +13,20 @@ public final class Text {
     public static String negotiation;
     public static String negotiationDescription;
 
+
     public static String workQuality;
     public static String workQualityDescription;
 
+    /**************************************************
+     * {@link CharacterInfo}
+     **************************************************/
     public static String sex;
     public static String sexMale;
     public static String sexFemale;
 
+    /**************************************************
+     * {@link SocietyClass}
+     **************************************************/
     public static String rich;
     public static String poor;
     public static String drunk;
@@ -24,5 +34,8 @@ public final class Text {
     public static String teenager;
     public static String student;
 
+    /**************************************************
+     * {@link TrafficHandler}
+     **************************************************/
     public static String setTrafficLoadException;
 }
