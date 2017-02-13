1cb57ffe18e6029a71cca46ec230811aae7f3094
masmix1 <4masmix1@gmail.com>
2017-02-13 03:33:38 +0100

    Add "validate" method

--- core/src/com/gpteam/shopmanager/Engine/Modules/Validator.java
+++ core/src/com/gpteam/shopmanager/Engine/Modules/Validator.java
@@ -1,8 +1,16 @@
 package com.gpteam.shopmanager.Engine.Modules;
 
+import static com.gpteam.shopmanager.Variables.Variables.MAX_TRAFFIC_LOAD;
+import static com.gpteam.shopmanager.Variables.Variables.MIN_TRAFFIC_LOAD;
+
 /*
  * Created by masmix on 13.02.2017.
  */
 public final class Validator {
-
+    private static boolean validate(int trafficLoad) {
+        if (trafficLoad > MIN_TRAFFIC_LOAD && trafficLoad < MAX_TRAFFIC_LOAD)
+            return true;
+        else
+            return false;
+    }
 }
