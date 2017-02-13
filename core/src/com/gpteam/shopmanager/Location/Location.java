e5513bcc1df24f36951a0710f291026482bd3e05
masmix1 <4masmix1@gmail.com>
2017-02-13 02:14:24 +0100

    Add societyClasses arraylist

--- core/src/com/gpteam/shopmanager/Location/Location.java
+++ core/src/com/gpteam/shopmanager/Location/Location.java
@@ -1,7 +1,13 @@
 package com.gpteam.shopmanager.Location;
 
+import com.gpteam.shopmanager.Society.SocietyClass;
+
+import java.util.ArrayList;
+
 /*
  * Created by masmix on 24.12.2016.
  */
 public class Location {
+    private ArrayList<SocietyClass> societyClasses;
+
 }
