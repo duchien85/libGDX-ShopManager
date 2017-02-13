a547eb11557ebaee185eff55afdd809c05f356f6
masmix1 <4masmix1@gmail.com>
2017-02-13 03:37:26 +0100

    Add constructor

--- core/src/com/gpteam/shopmanager/Traffic/TrafficHandler.java
+++ core/src/com/gpteam/shopmanager/Traffic/TrafficHandler.java
@@ -1,9 +1,14 @@
 package com.gpteam.shopmanager.Traffic;
 
+import com.gpteam.shopmanager.RandomGenerator.RandGen;
+
 /*
  * Created by masmix on 13.02.2017.
  */
 public class TrafficHandler {
     private int trafficLoad;
 
+    public TrafficHandler() {
+        trafficLoad = new RandGen().getTrafficLoad();
+    }
 }
