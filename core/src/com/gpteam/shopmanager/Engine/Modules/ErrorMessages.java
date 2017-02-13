2cefb1ea89588570391a6c565ac9df964a2668c8
masmix1 <4masmix1@gmail.com>
2017-02-13 03:46:11 +0100

    Add SET_TRAFFIC_LOAD_EXCEPTION_MESSAGE variable

--- core/src/com/gpteam/shopmanager/Engine/Modules/ErrorMessages.java
+++ core/src/com/gpteam/shopmanager/Engine/Modules/ErrorMessages.java
@@ -1,7 +1,10 @@
 package com.gpteam.shopmanager.Engine.Modules;
 
+import com.gpteam.shopmanager.Text.Text;
+
 /*
  * Created by masmix on 13.02.2017.
  */
 public final class ErrorMessages {
+    public static final String SET_TRAFFIC_LOAD_EXCEPTION_MESSAGE = Text.setTrafficLoadException;
 }
