ca6033ed459414c9a5c63745478578068939c524
masmix1 <4masmix1@gmail.com>
2017-02-13 04:04:09 +0100

    Add update method

--- core/src/com/gpteam/shopmanager/Engine/Modules/ErrorMessages.java
+++ core/src/com/gpteam/shopmanager/Engine/Modules/ErrorMessages.java
@@ -6,5 +6,11 @@ import com.gpteam.shopmanager.Text.Text;
  * Created by masmix on 13.02.2017.
  */
 public final class ErrorMessages {
-    public static final String SET_TRAFFIC_LOAD_EXCEPTION_MESSAGE = Text.setTrafficLoadException;
+    public static String SET_TRAFFIC_LOAD_EXCEPTION_MESSAGE = Text.setTrafficLoadException.equals;
+    
+    public static void update() {
+    				if(SET_TRAFFIC_LOAD_EXCEPTION_MESSAGE != Text.setTrafficLoadException)
+    						SET_TRAFFIC_LOAD_EXCEPTION_MESSAGE = Defaults.setTrafficLoadException;
+   
+    }
 }
