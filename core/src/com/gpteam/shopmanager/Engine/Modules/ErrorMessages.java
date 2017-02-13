18df560c87f02cb0709f6cdb7090658545d82c45
masmix1 <4masmix1@gmail.com>
2017-02-13 13:53:25 +0100

    Modify Text variable call

--- core/src/com/gpteam/shopmanager/Engine/Modules/ErrorMessages.java
+++ core/src/com/gpteam/shopmanager/Engine/Modules/ErrorMessages.java
@@ -6,6 +6,6 @@ import com.gpteam.shopmanager.Text.Text;
  * Created by masmix on 13.02.2017.
  */
 public final class ErrorMessages {
-    public static final String SET_TRAFFIC_LOAD_EXCEPTION_MESSAGE = Text.setTrafficLoadException;
+    public static final String SET_TRAFFIC_LOAD_EXCEPTION_MESSAGE = Text.SET_TRAFFIC_LOAD_EXCEPTION;
     public static final String VALIDATE_TRAFFIC_LOAD_EXCEPTION_MESSAGE = Text.validateTrafficLoadException;
 }
