c16a98c22687d4d39b6f4f86f6698fdca546f57f
masmix1 <4masmix1@gmail.com>
2017-02-13 02:02:55 +0100

    Add Hunger, apparel, happiness, luxury, fix, awarness

--- core/src/com/gpteam/shopmanager/Needs/Needs.java
+++ core/src/com/gpteam/shopmanager/Needs/Needs.java
@@ -4,5 +4,5 @@ package com.gpteam.shopmanager.Needs;
  * Created by masmix on 18.01.2017.
  */
 public enum Needs {
-    FOOD, DRINK, ENTERTAINMENT, CLOTHING, HAPPINES
+    HUNGER, ENTERTAINMENT, APPAREL, HAPPINESS, LUXURY, FIX, AWARNESS
 }
