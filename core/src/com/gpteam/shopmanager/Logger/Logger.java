f52aa1dd41579c70afcc3e285c87e0395cfc6443
masmix1 <4masmix1@gmail.com>
2017-02-14 23:26:29 +0100

    Add messages, message variables

--- core/src/com/gpteam/shopmanager/Logger/Logger.java
+++ core/src/com/gpteam/shopmanager/Logger/Logger.java
@@ -1,7 +1,13 @@
 package com.gpteam.shopmanager.Logger;
 
+import java.util.ArrayList;
+
 /*
  * Created by masmix on 14.02.2017.
  */
 public class Logger {
+    private ArrayList<String> messages;
+    private String message;
+
+
 }
