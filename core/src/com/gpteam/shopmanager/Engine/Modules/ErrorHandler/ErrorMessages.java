74a278d776296c666b449ed7a198c8df094db3be
masmix1 <4masmix1@gmail.com>
2017-02-17 14:51:11 +0100

    Rename VALIDATE_TRAFFIC_LOAD_EXCEPTION_MESSAGE variable

--- core/src/com/gpteam/shopmanager/Engine/Modules/ErrorHandler/ErrorMessages.java
+++ core/src/com/gpteam/shopmanager/Engine/Modules/ErrorHandler/ErrorMessages.java
@@ -1,10 +1,11 @@
 package com.gpteam.shopmanager.Engine.Modules.ErrorHandler;
 
 import com.gpteam.shopmanager.Text.Text;
+import com.gpteam.shopmanager.Variables.Variables;
 
 /*
  * Created by masmix on 13.02.2017.
  */
 public final class ErrorMessages {
-    public static final String VALIDATE_TRAFFIC_LOAD_EXCEPTION_MESSAGE = Text.VALIDATE_TRAFFIC_LOAD_EXCEPTION;
+    public static final String TRAFFIC_LOAD_EXCEPTION_MESSAGE = Text.VALIDATE_TRAFFIC_LOAD_EXCEPTION;
 }
