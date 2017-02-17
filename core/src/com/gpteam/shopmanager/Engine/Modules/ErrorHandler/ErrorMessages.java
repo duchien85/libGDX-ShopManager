ea3b67cb7c231d8cff166b23ff7e6d4388be3134
masmix1 <4masmix1@gmail.com>
2017-02-17 14:53:57 +0100

    Modify TRAFFIC_LOAD_EXCEPTION_MESSAGE string - initialize it right here

--- core/src/com/gpteam/shopmanager/Engine/Modules/ErrorHandler/ErrorMessages.java
+++ core/src/com/gpteam/shopmanager/Engine/Modules/ErrorHandler/ErrorMessages.java
@@ -7,5 +7,5 @@ import com.gpteam.shopmanager.Variables.Variables;
  * Created by masmix on 13.02.2017.
  */
 public final class ErrorMessages {
-    public static final String TRAFFIC_LOAD_EXCEPTION_MESSAGE = Text.VALIDATE_TRAFFIC_LOAD_EXCEPTION;
+    public static final String TRAFFIC_LOAD_EXCEPTION = "Traffic load must be between " + Variables.MIN_TRAFFIC_LOAD + " and " + Variables.MAX_TRAFFIC_LOAD + ". ";
 }
