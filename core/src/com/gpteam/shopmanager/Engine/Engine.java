9ea84d1fd0ee686c684f91d08a73cf36954af856
masmix1 <4masmix1@gmail.com>
2017-03-14 09:36:00 +0100

    Add "update" method

--- core/src/com/gpteam/shopmanager/Engine/Engine.java
+++ core/src/com/gpteam/shopmanager/Engine/Engine.java
@@ -27,4 +27,8 @@ public class Engine {
             ErrorHandler.handleNoSuchMethodException("No such method. Found: " + methodName);
         }
     }
+
+    public void update() {
+        economy.update();
+    }
 }
