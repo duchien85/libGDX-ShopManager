46fa5e4260bb5de056f3eccac0cf8289e3fb14e6
masmix1 <4masmix1@gmail.com>
2017-03-14 09:22:49 +0100

    Implement "Runnable" interface to "Logger" class with "run" method

--- core/src/com/gpteam/shopmanager/Logger/Logger.java
+++ core/src/com/gpteam/shopmanager/Logger/Logger.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 /*
  * Created by masmix on 14.02.2017.
  */
-public class Logger {
+public class Logger implements Runnable {
     private ArrayList<String> messages;
     private String message;
 
-
+    @Override
+    public void run() {
+    }
 }
