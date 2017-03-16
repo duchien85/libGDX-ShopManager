bad6b676cd9a225e34866054240ffc3911a29de9
masmix1 <4masmix1@gmail.com>
2017-03-16 12:59:20 +0100

    Add totalMoney, BigDecimal object

--- core/src/com/gpteam/shopmanager/Economy/Economy.java
+++ core/src/com/gpteam/shopmanager/Economy/Economy.java
@@ -4,6 +4,11 @@ package com.gpteam.shopmanager.Economy;
  * Created by masmix on 02.02.2017.
  */
 public class Economy {
+		private BigDecimal totalMoney;
     public void update() {
+    }
+    
+    public BigDecimal getTotalMoney() {
+    		return totalMoney;
     }
 }
