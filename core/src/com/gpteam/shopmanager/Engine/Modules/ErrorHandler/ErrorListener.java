4b34e082ea3680b8e33aa9614878e588c094709b
masmix1 <4masmix1@gmail.com>
2017-03-21 13:40:52 +0100

    Make errors ArrayList static

--- core/src/com/gpteam/shopmanager/Engine/Modules/ErrorHandler/ErrorListener.java
+++ core/src/com/gpteam/shopmanager/Engine/Modules/ErrorHandler/ErrorListener.java
@@ -1,12 +1,15 @@
 package com.gpteam.shopmanager.Engine.Modules.ErrorHandler;
 
+import java.util.ArrayList;
+
 /*
  * Created by masmix on 14.02.2017.
  */
 public final class ErrorListener {
     private static ErrorType errorType;
+    private static ArrayList<ErrorType> errors;
 
     public static void notify(ErrorType errorType) {
-
+        errors.add(errorType);
     }
 }
