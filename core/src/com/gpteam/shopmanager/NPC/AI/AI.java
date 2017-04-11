b4005c78eb844ed19ac75b3894628a5d0e94feb1
masmix1 <4masmix1@gmail.com>
2017-04-11 15:45:18 +0200

    Remove max hunger level variable

--- core/src/com/gpteam/shopmanager/NPC/AI/AI.java
+++ core/src/com/gpteam/shopmanager/NPC/AI/AI.java
@@ -4,6 +4,8 @@ package com.gpteam.shopmanager.NPC.AI;
  * Created by masmix on 18.01.2017.
  */
 public class AI {
+    private static final int MIN_HUNGER_LEVEL = 50;
+
     public void goToShop() {
     }
 
