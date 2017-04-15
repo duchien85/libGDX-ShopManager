4b4d68655918aaaf2293a354897a65d529580b15
masmix1 <4masmix1@gmail.com>
2017-04-15 12:45:41 +0200

    Make "Logger" class final

--- core/src/com/gpteam/shopmanager/Logger/Logger.java
+++ core/src/com/gpteam/shopmanager/Logger/Logger.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 /*
  * Created by masmix on 14.02.2017.
  */
-public class Logger {
+public final class Logger {
     private ArrayList<String> messages;
     private String message;
 }
