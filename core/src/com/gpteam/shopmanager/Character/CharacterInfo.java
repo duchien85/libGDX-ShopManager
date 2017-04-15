db7626984996a6f04cc9876e84309d277c63aef3
masmix1 <4masmix1@gmail.com>
2017-04-15 11:20:10 +0200

    Fix validate method

--- core/src/com/gpteam/shopmanager/Character/CharacterInfo.java
+++ core/src/com/gpteam/shopmanager/Character/CharacterInfo.java
@@ -98,7 +98,7 @@ public class CharacterInfo {
     }
 
     private boolean validate(String name) {
-        return name.length() < MAX_NAME_LENGTH && name.length() > MIN_NAME_LENGTH;
+        return name.length() <= MAX_NAME_LENGTH && name.length() >= MIN_NAME_LENGTH;
     }
 
     private boolean validate(int age) {
