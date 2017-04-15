2d08af182cd67b6b7dec14e98943a9c3afc43a17
masmix1 <4masmix1@gmail.com>
2017-04-15 11:31:17 +0200

    Make objects static

--- core/src/com/gpteam/shopmanager/FileHandlers/Loader.java
+++ core/src/com/gpteam/shopmanager/FileHandlers/Loader.java
@@ -9,9 +9,9 @@ import static com.gpteam.shopmanager.Variables.Variables.NAMES_PATH;
  * Created by masmix on 04.02.2017.
  */
 public final class Loader {
-    private File file;
-    private FileReader fileReader;
-    private BufferedReader bufferedReader;
+    private static File file;
+    private static FileReader fileReader;
+    private static BufferedReader bufferedReader;
 
     public void initialize() {
     }
