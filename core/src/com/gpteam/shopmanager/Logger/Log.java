b9e2c0802d61eefd4b18575a884f59d5acafe011
masmix1 <4masmix1@gmail.com>
2017-05-08 23:52:35 +0200

    Add Log clas

---
+++ core/src/com/gpteam/shopmanager/Logger/Log.java
@@ -0,0 +1,16 @@
+package com.gpteam.shopmanager.Logger;
+
+import com.gpteam.shopmanager.Engine.Modules.ErrorHandler.ErrorType;
+
+/*
+ * Created by masmix on 08.05.2017.
+ */
+public class Log {
+    private String[] log;
+    private ErrorType errorType;
+
+    public Log(String[] log, ErrorType errorType) {
+        this.log = log;
+        this.errorType = errorType;
+    }
+}
