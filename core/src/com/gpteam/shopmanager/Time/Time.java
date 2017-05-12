07cda78ae5a56b796bd2958681442121f2257f69
masmix1 <4masmix1@gmail.com>
2017-05-12 22:52:41 +0200

    Fix timeContainer, visual changes

--- core/src/com/gpteam/shopmanager/Time/Time.java
+++ core/src/com/gpteam/shopmanager/Time/Time.java
@@ -1,6 +1,6 @@
 package com.gpteam.shopmanager.Time;
 
 public final class Time {
-// liczenie czasu, metoda endOfDay()? ktora inkrementuje dzien do przodu
-private static int timeContainer[][][] = new int[24][60][60];
+    // liczenie czasu, metoda endOfDay()? ktora inkrementuje dzien do przodu
+    private static int timeContainer[][][] = new int[23][59][59];
 }
