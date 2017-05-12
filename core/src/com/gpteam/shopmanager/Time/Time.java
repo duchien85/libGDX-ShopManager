9ac59e8e2a8ef91c8a4efa411660ca5976fd4954
masmix1 <4masmix1@gmail.com>
2017-05-12 22:49:39 +0200

    Add package info

--- core/src/com/gpteam/shopmanager/Time/Time.java
+++ core/src/com/gpteam/shopmanager/Time/Time.java
@@ -1,3 +1,5 @@
+package com.gpteam.shopmanager.Time;
+
 public final class Time {
 // liczenie czasu, metoda endOfDay()? ktora inkrementuje dzien do przodu
 private static int timeContainer[][][] = new int[24][60][60];
