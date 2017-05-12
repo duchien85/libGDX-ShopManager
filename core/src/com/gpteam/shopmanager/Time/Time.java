615746bd366b4b849d8462813789972d734a7ed9
masmix1 <4masmix1@gmail.com>
2017-05-12 22:54:30 +0200

    Implement Runnable interface

--- core/src/com/gpteam/shopmanager/Time/Time.java
+++ core/src/com/gpteam/shopmanager/Time/Time.java
@@ -1,6 +1,10 @@
 package com.gpteam.shopmanager.Time;
 
-public final class Time {
+public final class Time implements Runnable {
     // liczenie czasu, metoda endOfDay()? ktora inkrementuje dzien do przodu
     private static int timeContainer[][][] = new int[23][59][59];
+
+    @Override
+    public void run() {
+    }
 }
