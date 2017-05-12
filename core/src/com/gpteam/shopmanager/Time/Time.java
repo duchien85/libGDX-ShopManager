2babc6d637117cc4899ebd4ac89d3ea926a0a872
masmix1 <4masmix1@gmail.com>
2017-05-12 22:56:37 +0200

    Add "date" variable

--- core/src/com/gpteam/shopmanager/Time/Time.java
+++ core/src/com/gpteam/shopmanager/Time/Time.java
@@ -1,8 +1,11 @@
 package com.gpteam.shopmanager.Time;
 
+import java.util.Date;
+
 public final class Time implements Runnable {
     // liczenie czasu, metoda endOfDay()? ktora inkrementuje dzien do przodu
     private static int timeContainer[][][] = new int[23][59][59];
+    private static Date date;
 
     @Override
     public void run() {
