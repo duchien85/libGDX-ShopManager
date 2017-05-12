e511db6e5d954fb52e7adc672633f3065390e660
masmix1 <4masmix1@gmail.com>
2017-05-12 23:13:17 +0200

    Remove timeContainer, unnecessary

--- core/src/com/gpteam/shopmanager/Time/Time.java
+++ core/src/com/gpteam/shopmanager/Time/Time.java
@@ -6,7 +6,6 @@ import java.util.Date;
 
 public final class Time implements Runnable {
     // liczenie czasu, metoda endOfDay()? ktora inkrementuje dzien do przodu
-    private static int timeContainer[][][] = new int[23][59][59];
     private static DateTime date = new DateTime();
 
     @Override
