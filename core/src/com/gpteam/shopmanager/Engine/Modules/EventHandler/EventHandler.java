c35497b8f2190b57d21040e666ac5a1dbe06d88c
masmix1 <4masmix1@gmail.com>
2017-05-13 15:17:16 +0200

    Add "getNext" method

--- core/src/com/gpteam/shopmanager/Engine/Modules/EventHandler/EventHandler.java
+++ core/src/com/gpteam/shopmanager/Engine/Modules/EventHandler/EventHandler.java
@@ -1,14 +1,20 @@
 package com.gpteam.shopmanager.Engine.Modules.EventHandler;
 
 import java.util.ArrayList;
+import java.util.TreeSet;
 
 /*
  * Created by masmix on 13.05.2017.
  */
 public final  class EventHandler {
     private static ArrayList<Event> events = new ArrayList<Event>();
+    private static int index = 0;
 
     public static void add(Event event) {
         events.add(event);
+    }
+
+    public static Event getNext() {
+        return events.get(index++);
     }
 }
