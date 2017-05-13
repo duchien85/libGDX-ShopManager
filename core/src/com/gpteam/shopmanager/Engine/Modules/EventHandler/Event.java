ab4419b86a2f71d548d95c06cf5456f737862c88
masmix1 <4masmix1@gmail.com>
2017-05-13 15:00:51 +0200

    Add "EventHandler" module with "Event" and "EventHandler" classes

---
+++ core/src/com/gpteam/shopmanager/Engine/Modules/EventHandler/Event.java
@@ -0,0 +1,16 @@
+package com.gpteam.shopmanager.Engine.Modules.EventHandler;
+
+/*
+ * Created by masmix on 13.05.2017.
+ */
+public class Event {
+    private String text;
+
+    public Event(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
---
+++ core/src/com/gpteam/shopmanager/Engine/Modules/EventHandler/EventHandler.java
@@ -0,0 +1,7 @@
+package com.gpteam.shopmanager.Engine.Modules.EventHandler;
+
+/*
+ * Created by masmix on 13.05.2017.
+ */
+public class EventHandler {
+}
