3855ea48b085f26cdfe183cd85c94eb1ce2e0902
masmix1 <4masmix1@gmail.com>
2017-05-14 23:39:54 +0200

    Swap "DateTime" for my "Date" class

--- core/src/com/gpteam/shopmanager/Time/Time.java
+++ core/src/com/gpteam/shopmanager/Time/Time.java
@@ -1,13 +1,13 @@
 package com.gpteam.shopmanager.Time;
 
 import com.gpteam.shopmanager.Variables.Variables;
-import org.joda.time.DateTime;
 
 public final class Time implements Runnable {
-    private static DateTime date;
+    private static Date date;
 
     @Override
     public void run() {
+        date = new Date();
         for (int i = 1; i <= 24; i++) {
             for (int j = 1; j <= 60; j++) {
                 for (int k = 1; k <= Variables.SECONDS; k++) {
@@ -28,6 +28,5 @@ public final class Time implements Runnable {
     }
 
     public static void initialize() {
-        date = new DateTime();
     } // TODO Add start date
 }
