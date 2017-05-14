3d94949f2b997a44b51fe4c06106fb08740218db
masmix1 <4masmix1@gmail.com>
2017-05-15 01:06:28 +0200

    Add "isHungry" boolean

--- core/src/com/gpteam/shopmanager/NPC/AI/AI.java
+++ core/src/com/gpteam/shopmanager/NPC/AI/AI.java
@@ -6,6 +6,8 @@ package com.gpteam.shopmanager.NPC.AI;
 public class AI {
     private static final int MIN_HUNGER_LEVEL = 50;
 
+    private boolean isHungry = false;
+
     public void goShopping() {
     }
 
