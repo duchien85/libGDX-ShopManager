d6c0995fdb5abd1fc3f9effe652e375baaf0a01c
masmix1 <4masmix1@gmail.com>
2017-05-14 13:05:38 +0200

    Add "ProductView" class

---
+++ core/src/com/gpteam/shopmanager/Product/ProductView.java
@@ -0,0 +1,12 @@
+package com.gpteam.shopmanager.Product;
+
+/*
+ * Created by masmix on 14.05.2017.
+ */
+public class ProductView {
+    private String serialName;
+
+    public String getSerialName() {
+        return serialName;
+    }
+}
