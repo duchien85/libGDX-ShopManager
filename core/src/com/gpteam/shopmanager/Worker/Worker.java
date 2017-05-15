abcf07f201b8275c222d140570cbc8d763f3f34f
masmix1 <4masmix1@gmail.com>
2017-05-15 15:35:33 +0200

    Add characterInfo, expertise

--- core/src/com/gpteam/shopmanager/Worker/Worker.java
+++ core/src/com/gpteam/shopmanager/Worker/Worker.java
@@ -1,7 +1,14 @@
 package com.gpteam.shopmanager.Worker;
 
+import com.gpteam.shopmanager.Character.CharacterInfo;
+import com.gpteam.shopmanager.Skill.Skills.Expertise;
+
 /*
  * Created by masmix on 15.05.2017.
  */
 public class Worker {
+    private CharacterInfo characterInfo;
+
+    private Expertise expertise;
+
 }
