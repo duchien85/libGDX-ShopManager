7a973ffb81fb3e38098a0a6fedad9f7ab92abb33
masmix1 <4masmix1@gmail.com>
2017-05-15 18:30:09 +0200

    Add private constructor

--- core/src/com/gpteam/shopmanager/Loader/Loader.java
+++ core/src/com/gpteam/shopmanager/Loader/Loader.java
@@ -9,6 +9,9 @@ import static com.gpteam.shopmanager.Variables.Variables.NAMES_PATH;
  * Created by masmix on 04.02.2017.
  */
 public final class Loader {
+			private Loader() {
+					ErrorHandler.handleUnsupportedOperationException();
+			}
     private static File file;
     private static FileReader fileReader;
     private static BufferedReader bufferedReader;
