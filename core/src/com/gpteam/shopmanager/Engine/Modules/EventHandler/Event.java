0d4cfdb21ade6fc8d1ec8bc857060692a4a90e62
masmix1 <gabriel.wawerski@gmail.com>
2017-05-18 17:45:02 +0200

    Modify constructor

--- core/src/com/gpteam/shopmanager/Engine/Modules/EventHandler/Event.java
+++ core/src/com/gpteam/shopmanager/Engine/Modules/EventHandler/Event.java
@@ -4,10 +4,12 @@ package com.gpteam.shopmanager.Engine.Modules.EventHandler;
  * Created by masmix on 13.05.2017.
  */
 public class Event {
-    private String text;
+    private String[] text;
 
-    public Event(String text) {
-        this.text = text;
+    public Event(String class, String text) {
+        this.text = new String[2];
+        this.text[0] = class;
+        this.text[1] = text;
     }
 
     public String getText() {
