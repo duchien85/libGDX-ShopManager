1745a84c6cdc7aa2339dcd7fc1481ec08aebbb3a
masmix1 <4masmix1@gmail.com>
2017-06-25 01:05:59 +0200

    Modify constructor

--- core/src/com/gpteam/shopmanager/Logger/Log.java
+++ core/src/com/gpteam/shopmanager/Logger/Log.java
@@ -1,6 +1,7 @@
 package com.gpteam.shopmanager.Logger;
 
 import com.gpteam.shopmanager.Engine.Modules.ErrorHandler.ErrorType;
+import com.gpteam.shopmanager.Engine.Modules.EventHandler.IEvent;
 
 /*
  * Created by masmix on 08.05.2017.
@@ -9,9 +10,10 @@ public class Log {
     private String[] log;
     private ErrorType errorType;
 
-    public Log(Loggable log) {
-        this.log = log.log;
-        this.errorType = log.errorType;
+    public Log(String className, String message) {
+        this.log = new String[2];
+        this.log[0] = className;
+        this.log[1] = message;
     }
 
     public String[] getLog() {
