2f87ec5104abd18f636da6348d60a28618b3fc07
masmix1 <4masmix1@gmail.com>
2017-07-01 17:11:39 +0200

    Add TODO's

--- core/src/com/gpteam/shopmanager/City/City.java
+++ core/src/com/gpteam/shopmanager/City/City.java
@@ -9,4 +9,7 @@ public class City {
     private String name;
     private int population;
     private Area[] areas;
+
+    // TODO figure out how to handle assigning npcs to
+    // TODO individual areas of the city
 }
