e9c6c9e6cf6f61781625459cf16fa218628d7eac
masmix1 <4masmix1@gmail.com>
2017-07-12 19:08:42 +0200

    Fix Month value

--- core/src/com/gpteam/shopmanager/time/Date.java
+++ core/src/com/gpteam/shopmanager/time/Date.java
@@ -29,7 +29,7 @@ public class Date {
 
     @Override
     public String toString() {
-        return calendar.get(Calendar.DAY_OF_MONTH) + "." + calendar.get(Calendar.MONTH) + "." + calendar.get(Calendar.YEAR);
+        return calendar.get(Calendar.DAY_OF_MONTH) + "." + calendar.get(Calendar.MONTH + 1) + "." + calendar.get(Calendar.YEAR);
     }
 
     public long getTime() {
