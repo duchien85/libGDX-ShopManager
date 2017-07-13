826a593511c87ca98df5f11146919d33cd189f6f
masmix1 <4masmix1@gmail.com>
2017-07-13 14:01:38 +0200

    Remove unused field

--- core/src/com/gpteam/shopmanager/engine/modules/error_handler/ErrorListener.java
+++ core/src/com/gpteam/shopmanager/engine/modules/error_handler/ErrorListener.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
  * Created by masmix on 14.02.2017.
  */
 public final class ErrorListener {
-    private static ErrorType errorType;
     private static ArrayList<ErrorType> errors;
 
     public static void notify(ErrorType errorType) {
