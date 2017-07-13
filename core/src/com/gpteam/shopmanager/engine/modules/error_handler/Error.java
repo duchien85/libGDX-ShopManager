8d1c2972deffc56506e235f3a02df7eea188f46d
masmix1 <4masmix1@gmail.com>
2017-07-13 15:02:11 +0200

    Add constructor, getters

--- core/src/com/gpteam/shopmanager/engine/modules/error_handler/Error.java
+++ core/src/com/gpteam/shopmanager/engine/modules/error_handler/Error.java
@@ -4,4 +4,22 @@ public class Error {
     private String className;
     private String errorMessage;
     private ErrorType errorType;
+
+    public Error(String className, String errorMessage, ErrorType errorType) {
+        this.className = className;
+        this.errorMessage = errorMessage;
+        this.errorType = errorType;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public ErrorType getErrorType() {
+        return errorType;
+    }
 }
