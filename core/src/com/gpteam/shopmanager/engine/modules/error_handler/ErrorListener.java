f36245c37125ee2e9245fa60fb28dbbe1a3aa7dc
masmix1 <4masmix1@gmail.com>
2017-07-13 15:03:42 +0200

    Make use of "Error" class

--- core/src/com/gpteam/shopmanager/engine/modules/error_handler/ErrorListener.java
+++ core/src/com/gpteam/shopmanager/engine/modules/error_handler/ErrorListener.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
  * Created by masmix on 14.02.2017.
  */
 public final class ErrorListener {
-    private static ArrayList<ErrorType> errors;
+    private static ArrayList<Error> errors;
 
-    public static void notify(ErrorType errorType) {
-        errors.add(errorType);
+    public static void notify(Error error) {
+        errors.add(error);
     }
 }
