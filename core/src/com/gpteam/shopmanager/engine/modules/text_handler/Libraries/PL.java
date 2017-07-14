14dcf5b739d0d7589cbfd22b5322d791d6641553
masmix1 <4masmix1@gmail.com>
2017-07-14 18:49:42 +0200

    Replace direct call to import

--- core/src/com/gpteam/shopmanager/engine/modules/text_handler/Libraries/PL.java
+++ core/src/com/gpteam/shopmanager/engine/modules/text_handler/Libraries/PL.java
@@ -1,36 +1,38 @@
 package com.gpteam.shopmanager.engine.modules.text_handler.Libraries;
 
+import com.gpteam.shopmanager.engine.modules.text_handler.Text;
+
 /*
  * Created by masmix on 18.01.2017.
  */
 public final class PL {
     public static void initialize() {
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.expertise = "Ekspertyza";
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.expertiseDescription = "";
+        Text.expertise = "Ekspertyza";
+        Text.expertiseDescription = "";
 
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.negotiation = "Negocjacja";
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.negotiationDescription = "";
+        Text.negotiation = "Negocjacja";
+        Text.negotiationDescription = "";
 
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.workQuality = "Jakość pracy";
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.workQualityDescription = "";
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.sex = "Płeć";
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.sexMale = "Mężczyzna";
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.sexFemale = "Kobieta";
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.ERROR_CONSTRUCTOR = "Jeden z argumentów był niewłaściwy";
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.ERROR_CONSTRUCTOR_AGE = ", wiek: ";
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.ERROR_CONSTRUCTOR_SEX = ", płeć: ";
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.ERROR_CHARACTER_INFO = "";
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.ERROR_SET_NAME = "Imię jest za krótkie lub za długie. Imię musi posiadać co najmniej 2 litery, do 25";
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.ERROR_SET_AGE = "Nieprawidłowy wiek. Wiek powinien zawierać się między 12 a 120";
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.ERROR_SET_SEX = "Nieprawidłowa płeć";
+        Text.workQuality = "Jakość pracy";
+        Text.workQualityDescription = "";
+        Text.sex = "Płeć";
+        Text.sexMale = "Mężczyzna";
+        Text.sexFemale = "Kobieta";
+        Text.ERROR_CONSTRUCTOR = "Jeden z argumentów był niewłaściwy";
+        Text.ERROR_CONSTRUCTOR_AGE = ", wiek: ";
+        Text.ERROR_CONSTRUCTOR_SEX = ", płeć: ";
+        Text.ERROR_CHARACTER_INFO = "";
+        Text.ERROR_SET_NAME = "Imię jest za krótkie lub za długie. Imię musi posiadać co najmniej 2 litery, do 25";
+        Text.ERROR_SET_AGE = "Nieprawidłowy wiek. Wiek powinien zawierać się między 12 a 120";
+        Text.ERROR_SET_SEX = "Nieprawidłowa płeć";
 
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.rich = "Bogaci";
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.poor = "Biedni";
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.drunk = "Pijacy";
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.middleclass = "Klasa Srednia";
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.teenager = "Nastolatkowie";
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.student = "Studenci";
+        Text.rich = "Bogaci";
+        Text.poor = "Biedni";
+        Text.drunk = "Pijacy";
+        Text.middleclass = "Klasa Srednia";
+        Text.teenager = "Nastolatkowie";
+        Text.student = "Studenci";
 
-        com.gpteam.shopmanager.engine.modules.text_handler.Text.ERROR_MESSAGE_1 = "";
+        Text.ERROR_MESSAGE_1 = "";
     }
 }
