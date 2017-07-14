6795a042d64e7e0f51a1a218db2e9d9dad3166f2
masmix1 <4masmix1@gmail.com>
2017-07-14 18:19:16 +0200

    Swap BigDecimal to float in "capital" field

--- core/src/com/gpteam/shopmanager/company/Company.java
+++ core/src/com/gpteam/shopmanager/company/Company.java
@@ -1,9 +1,7 @@
 package com.gpteam.shopmanager.company;
 
-import java.math.BigDecimal;
-
 public class Company {
     private String shortName;
     private String fullName;
-    private BigDecimal capital;
+    private float capital;
 }
