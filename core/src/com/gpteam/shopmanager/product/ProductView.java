930c5adc2fe3d81c8f7db1a8ad163af02fbc18ab
masmix1 <4masmix1@gmail.com>
2017-07-16 14:39:12 +0200

    Modify javadoc

--- core/src/com/gpteam/shopmanager/product/ProductView.java
+++ core/src/com/gpteam/shopmanager/product/ProductView.java
@@ -4,18 +4,18 @@ import com.gpteam.shopmanager.engine.modules.error_handler.ErrorHandler;
 import com.gpteam.shopmanager.engine.modules.text_handler.Text;
 
 /**
- * Created by masmix on 14.05.2017.
+ * Created by masmix on 14.05.2017.<p>
  *
  * ProductView class that contains the information on all products in the game.
  * That information is stored in individual arrays named as the product name.
- * Each index contains different info on the product from {@link Text} class.
- * Use this class to create product info more easily.
- * It goes as follows:
- *                                                         example:
- * index 0: product name                                   productName
- * index 1: product serial name                            PRODUCT_SERIALNAME
- * index 2: product description                            productDescription
- * --------------------------------------------------------------------------
+ * Each index contains different info on the product from {@link Text} class.<p>
+ * Use this class to create {@link Product} class objects more easily.<p>
+ * It goes as follows:<p>
+ *                                                         example:<p>
+ * index 0: product name                                   productName<p>
+ * index 1: product serial name                            PRODUCT_SERIALNAME<p>
+ * index 2: product description                            productDescription<p>
+ * --------------------------------------------------------------------------<p>
  * Class must be initialized after TextHandler class!
  */
 public final class ProductView {
