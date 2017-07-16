e4dd4a82831329ffe110c70b3bcc7ddf8c7057ee
masmix1 <4masmix1@gmail.com>
2017-07-16 03:23:07 +0200

    Add more javadoc info

--- core/src/com/gpteam/shopmanager/product/ProductView.java
+++ core/src/com/gpteam/shopmanager/product/ProductView.java
@@ -5,8 +5,17 @@ import com.gpteam.shopmanager.engine.modules.text_handler.Text;
 
 /**
  * Created by masmix on 14.05.2017.
-
- * Class must be initialized AFTER TextHandler class
+ *
+ * ProductView class that contains the information on all products in the game.
+ * That information is stored in individual arrays named after the product.
+ * Each index contains different info on the product from {@link Text} class.
+ * It goes as follows:
+ *                                                         example:
+ * index 0: product name                                   productName
+ * index 1: product serial name                            PRODUCT_SERIALNAME
+ * index 2: product description                            productDescription
+ * --------------------------------------------------------------------------
+ * Class must be initialized after TextHandler class!
  */
 public final class ProductView {
     public static String[] APPLE;
