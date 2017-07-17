52576487410c45f1be93a15ddd447a958b429abb
masmix1 <4masmix1@gmail.com>
2017-07-18 01:32:27 +0200

    Move "district" package to "deprecated" package

--- core/src/com/gpteam/shopmanager/deprecated/district/District.java
+++ core/src/com/gpteam/shopmanager/deprecated/district/District.java
@@ -1,4 +1,6 @@
-package com.gpteam.shopmanager.city.district;
+package com.gpteam.shopmanager.deprecated.district;
+
+import com.gpteam.shopmanager.npc.NPCHandler;
 
 /*
  * Created by masmix on 28.01.2017.
@@ -6,11 +8,12 @@ package com.gpteam.shopmanager.city.district;
 public class District {
     private String areaName;
     private int population;
+    private NPCHandler npcHandler;
 
     private static int MIN_POPULATION_AMOUNT = 0;
     private static int MAX_POPULATION_AMOUNT = 5000;
 
-    public District(String areaName) {
+    public District(String areaName, int population) {
         this.areaName = areaName;
     }
 
