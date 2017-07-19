fcd2688dff945aba472904123f0515567532576d
masmix1 <4masmix1@gmail.com>
2017-07-19 19:43:16 +0200

    Add shops adding to private field

--- core/src/com/gpteam/shopmanager/company/detailer/Detailer.java
+++ core/src/com/gpteam/shopmanager/company/detailer/Detailer.java
@@ -4,6 +4,7 @@ import com.gpteam.shopmanager.company.Company;
 import com.gpteam.shopmanager.shop.Shop;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Detailer extends Company {
     // default product-selling company - Detailer
@@ -13,5 +14,6 @@ public class Detailer extends Company {
     public Detailer(String shortName, String fullName, float capital, Shop... shops) {
         super(shortName, fullName);
         this.capital = capital;
+        this.shops.addAll(Arrays.asList(shops));
     }
 }
