57923e05f3130f05f67662d6f1d7870ce25e3d7f
masmix1 <4masmix1@gmail.com>
2017-07-19 19:09:07 +0200

    Add constructor

--- core/src/com/gpteam/shopmanager/company/Company.java
+++ core/src/com/gpteam/shopmanager/company/Company.java
@@ -3,4 +3,9 @@ package com.gpteam.shopmanager.company;
 public class Company {
     private String shortName;
     private String fullName;
+
+    public Company(String shortName, String fullName) {
+        this.shortName = shortName;
+        this.fullName = fullName;
+    }
 }
