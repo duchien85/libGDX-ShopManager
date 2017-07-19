da323056abe07a71c6c4270f0dd9a8c63553ea7c
masmix1 <4masmix1@gmail.com>
2017-07-19 19:09:16 +0200

    Add constructor

--- core/src/com/gpteam/shopmanager/company/detailer/Detailer.java
+++ core/src/com/gpteam/shopmanager/company/detailer/Detailer.java
@@ -9,4 +9,9 @@ public class Detailer extends Company {
     // default product-selling company - Detailer
     private ArrayList<Shop> shops;
     private float capital;
+
+    public Detailer(String shortName, String fullName, float capital, Shop... shops) {
+        super(shortName, fullName);
+        this.capital = capital;
+    }
 }
