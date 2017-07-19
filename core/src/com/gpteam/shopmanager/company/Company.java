4dcd90a2c04ec2498bc2f87e0686739ccb3afc5b
masmix1 <4masmix1@gmail.com>
2017-07-19 19:39:59 +0200

    Add getters, setters

--- core/src/com/gpteam/shopmanager/company/Company.java
+++ core/src/com/gpteam/shopmanager/company/Company.java
@@ -8,4 +8,21 @@ public class Company {
         this.shortName = shortName;
         this.fullName = fullName;
     }
+
+    // TODO add constraints
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 }
