408955a027e5d9e3b804ea2e458b9ef25633e6d9
masmix1 <4masmix1@gmail.com>
2017-07-19 16:09:11 +0200

    Add "shops", "capital" fields, extend Company, add TODO

--- core/src/com/gpteam/shopmanager/company/detailer/Detailer.java
+++ core/src/com/gpteam/shopmanager/company/detailer/Detailer.java
@@ -1,4 +1,12 @@
 package com.gpteam.shopmanager.company.detailer;
 
-public class Detailer {
+import com.gpteam.shopmanager.company.Company;
+import com.gpteam.shopmanager.shop.Shop;
+
+import java.util.ArrayList;
+
+public class Detailer extends Company {
+    // TODO default product-selling company
+    private ArrayList<Shop> shops;
+    private float capital;
 }
