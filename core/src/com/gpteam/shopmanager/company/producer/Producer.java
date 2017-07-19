c426a2fcefe43eb699b12d1c946f82173603f194
masmix1 <4masmix1@gmail.com>
2017-07-19 19:13:19 +0200

    Add constructor

--- core/src/com/gpteam/shopmanager/company/producer/Producer.java
+++ core/src/com/gpteam/shopmanager/company/producer/Producer.java
@@ -5,4 +5,9 @@ import com.gpteam.shopmanager.company.Company;
 public class Producer extends Company {
     // TODO certain production speed (product/sec or min) that may be improved (along with product quality)
     private double productionSpeed;
+
+    public Producer(String shortName, String fullName) {
+        super(shortName, fullName);
+        productionSpeed = 1;
+    }
 }
