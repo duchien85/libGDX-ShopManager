6b359531772181465332cb4cbfad7fc72d28677a
masmix1 <4masmix1@gmail.com>
2017-07-21 02:02:01 +0200

    Fix BigDecimal object instantiation and set it up

--- core/src/com/gpteam/shopmanager/company/detailer/Detailer.java
+++ core/src/com/gpteam/shopmanager/company/detailer/Detailer.java
@@ -4,6 +4,7 @@ import com.gpteam.shopmanager.company.Company;
 import com.gpteam.shopmanager.shop.Shop;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -12,9 +13,11 @@ public class Detailer extends Company {
     private ArrayList<Shop> shops;
     private BigDecimal capital;
 
-    public Detailer(String shortName, String fullName, BigDecimal capital, Shop... shops) {
+    public Detailer(String shortName, String fullName, String capital, Shop... shops) {
         super(shortName, fullName);
-        this.capital = capital;
+        this.capital = new BigDecimal(capital, new MathContext(30));
+        this.capital = this.capital.setScale(2, BigDecimal.ROUND_HALF_UP);
+        this.shops = new ArrayList<Shop>(shops.length);
         this.shops.addAll(Arrays.asList(shops));
     }
 }
