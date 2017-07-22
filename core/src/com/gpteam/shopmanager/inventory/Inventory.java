7c2990b4fe6b80fc349299fdd6fc511d315105f8
masmix1 <4masmix1@gmail.com>
2017-07-22 06:47:43 +0200

    Swap conditions in if statement

--- core/src/com/gpteam/shopmanager/inventory/Inventory.java
+++ core/src/com/gpteam/shopmanager/inventory/Inventory.java
@@ -78,7 +78,7 @@ public class Inventory {
     }
 
     public void addProductPrice(String pVSerialName, String amount) {
-        if (Integer.valueOf(amount) >= 0 && validate(pVSerialName))
+        if (validate(pVSerialName) && Integer.valueOf(amount) >= 0)
             products.get(pVSerialName).addPrice(amount);
         else
             ErrorHandler.handleIllegalArgumentException("msg");
