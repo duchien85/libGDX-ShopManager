31838c0baa7023eb252734989c3bb6c7e44d54ec
masmix1 <4masmix1@gmail.com>
2017-07-26 03:46:12 +0200

    Add Advertising class with methods

---
+++ core/src/com/gpteam/shopmanager/advertising/Advertising.java
@@ -0,0 +1,33 @@
+package com.gpteam.shopmanager.advertising;
+
+import java.math.BigDecimal;
+
+public final class Advertising {
+
+    private String funds;
+        /**
+         * Method that returns most possible outcome of the given
+         * increase or decrease of ads funding
+         * @return
+         */
+    public String[] estimateOutcome(String funds) {
+        return null;
+    }
+
+    public String getStringAdsRevenue() {
+        return null;
+    }
+
+    public int getIntAdsRevenue() {
+        return 0;
+    }
+
+    public void setFunds(String funds) {
+    }
+
+    public void increaseFunds(String funds) {
+    }
+
+    public void decreaseFunds(String funds) {
+    }
+}
