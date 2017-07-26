670b3d2e370503f9bb68d151a1e95ec1c1998679
masmix1 <4masmix1@gmail.com>
2017-07-26 18:00:52 +0200

    Make AdSource interface an abstract class

--- core/src/com/gpteam/shopmanager/advertising/sources/AdSource.java
+++ core/src/com/gpteam/shopmanager/advertising/sources/AdSource.java
@@ -4,24 +4,31 @@ import com.gpteam.shopmanager.society.SocietyClass;
 
 import java.math.BigDecimal;
 
-public interface AdSource {
+public abstract class AdSource {
     /**
      * Increase funding of ads,
      */
-    public void increaseFunds(String funds);
+    public void increaseFunds(String funds) {
+    }
 
-    public void decreaseFunds(String funds);
+    public void decreaseFunds(String funds) {
+    }
 
-    public void setFunds(String funds);
+    public void setFunds(String funds) {
+    }
 
     /**
      * Get the amount of bots that have been influenced
      * by ads, only from desired society class.
      */
-    public int getSocietyClassEffectiveness(SocietyClass societyClass);
+    public int getSocietyClassEffectiveness(SocietyClass societyClass) {
+        return 0;
+    }
 
     /**
      * Get the amount of total bots influenced by ads.
      */
-    public int getAdEffectiveness();
+    public int getAdEffectiveness() {
+        return 0;
+    }
 }
