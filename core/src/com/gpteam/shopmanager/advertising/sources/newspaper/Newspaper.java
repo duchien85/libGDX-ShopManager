8a45802cdf8e9907a58c743dbd28d24487079042
masmix1 <4masmix1@gmail.com>
2017-08-10 22:12:12 +0200

    Fix minor errors

--- core/src/com/gpteam/shopmanager/advertising/sources/newspaper/Newspaper.java
+++ core/src/com/gpteam/shopmanager/advertising/sources/newspaper/Newspaper.java
@@ -3,17 +3,14 @@ package com.gpteam.shopmanager.advertising.sources.newspaper;
 import com.gpteam.shopmanager.advertising.sources.AdSource;
 import com.gpteam.shopmanager.society.SocietyClass;
 
-public class Newspaper implements AdSource {
-    @Override
+public class Newspaper extends AdSource {
     public void increaseFunds() {
     }
 
-    @Override
     public int getSocietyClassEffectivnes(SocietyClass societyClass) {
         return 0;
     }
 
-    @Override
     public void getAdEffectivnes() {
     }
 }
