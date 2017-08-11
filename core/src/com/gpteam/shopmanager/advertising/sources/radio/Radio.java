be8b2652f3d10d4acd7074eb52349c63e0fee23e
masmix1 <4masmix1@gmail.com>
2017-08-11 05:03:56 +0200

    Add super constructor

--- core/src/com/gpteam/shopmanager/advertising/sources/radio/Radio.java
+++ core/src/com/gpteam/shopmanager/advertising/sources/radio/Radio.java
@@ -3,4 +3,7 @@ package com.gpteam.shopmanager.advertising.sources.radio;
 import com.gpteam.shopmanager.advertising.sources.AdSource;
 
 public class Radio extends AdSource {
+    public Radio(String funds, String MIN_FUNDS, String MAX_FUNDS) {
+        super(funds, MIN_FUNDS, MAX_FUNDS);
+    }
 }
