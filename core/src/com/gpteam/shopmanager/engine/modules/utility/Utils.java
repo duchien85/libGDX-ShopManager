70fa2d976036a0a970c0e564a51eb3dfcfc8f263
masmix1 <4masmix1@gmail.com>
2017-08-11 04:40:00 +0200

    Add TODO

--- core/src/com/gpteam/shopmanager/engine/modules/utility/Utils.java
+++ core/src/com/gpteam/shopmanager/engine/modules/utility/Utils.java
@@ -3,6 +3,7 @@ package com.gpteam.shopmanager.engine.modules.utility;
 import java.math.BigDecimal;
 
 public final class Utils {
+    /** TODO Write wrapper class for BigDecimal, similiar to {@link com.gpteam.shopmanager.time.Date} wrapper class */
     public static void setScale(BigDecimal bigDecimal, int scale) {
         bigDecimal = bigDecimal.setScale(scale, BigDecimal.ROUND_HALF_UP);
     }
