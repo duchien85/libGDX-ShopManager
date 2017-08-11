9a50257eb23b246c6f8f48ce9780b6e346e8e732
masmix1 <4masmix1@gmail.com>
2017-08-11 17:25:27 +0200

    Add checking for null in contains method

--- core/src/com/gpteam/shopmanager/advertising/AdSources.java
+++ core/src/com/gpteam/shopmanager/advertising/AdSources.java
@@ -1,9 +1,14 @@
 package com.gpteam.shopmanager.advertising;
 
+import com.gpteam.shopmanager.engine.modules.error_handler.ErrorHandler;
+
 public enum AdSources {
     NEWSPAPER, RADIO, TV;
 
     public static boolean contains(AdSources adSource) {
+        if (adSource == null)
+            ErrorHandler.handleNullPointerException("msg");
+
         for (AdSources x : AdSources.values()) {
             if (x.name().contains(adSource.name()))
                 return true;
