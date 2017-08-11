483bbd916804b57ff5912ee47dd2b22497c00775
masmix1 <4masmix1@gmail.com>
2017-08-11 04:38:24 +0200

    Add constructor

--- core/src/com/gpteam/shopmanager/advertising/sources/AdSource.java
+++ core/src/com/gpteam/shopmanager/advertising/sources/AdSource.java
@@ -1,6 +1,7 @@
 package com.gpteam.shopmanager.advertising.sources;
 
 import com.gpteam.shopmanager.engine.modules.error_handler.ErrorHandler;
+import com.gpteam.shopmanager.engine.modules.utility.Utils;
 import com.gpteam.shopmanager.society.SocietyClass;
 
 import java.math.BigDecimal;
@@ -10,6 +11,16 @@ import java.math.RoundingMode;
 public abstract class AdSource {
     private BigDecimal funds;
     private int adEffectiveness;
+
+    private final String MIN_FUNDS;
+    private final String MAX_FUNDS;
+
+    public AdSource(String funds, String MIN_FUNDS, String MAX_FUNDS) {
+        this.funds = new BigDecimal(funds, new MathContext(30, RoundingMode.HALF_UP));
+        Utils.setScale(this.funds, 2);
+        this.MIN_FUNDS = MIN_FUNDS;
+        this.MAX_FUNDS = MAX_FUNDS;
+    }
 
     /**
      * Increase funding of ads,
