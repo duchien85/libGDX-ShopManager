395a466e3b9cc0e07e656f51192657bbf2ac4fcd
masmix1 <4masmix1@gmail.com>
2017-08-11 15:16:38 +0200

    Add handleNoSuchElementException method

--- core/src/com/gpteam/shopmanager/engine/modules/error_handler/ErrorHandler.java
+++ core/src/com/gpteam/shopmanager/engine/modules/error_handler/ErrorHandler.java
@@ -2,6 +2,8 @@ package com.gpteam.shopmanager.engine.modules.error_handler;
 
 import com.gpteam.shopmanager.engine.interfaces.Module;
 
+import java.util.NoSuchElementException;
+
 /*
  * Created by masmix on 11.02.2017.
  */
@@ -26,5 +28,9 @@ public final class ErrorHandler implements Module {
     public static void handleNoSuchFieldException(String message) {
         try { throw new NoSuchFieldException(message); }
         catch (NoSuchFieldException e) { e.printStackTrace(); }
+    }
+
+    public static void handleNoSuchElementException(String msg) {
+        throw new NoSuchElementException("msg");
     }
 }
