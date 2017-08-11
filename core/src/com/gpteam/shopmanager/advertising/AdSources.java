9837413c865bbae786aa0c931ed8a7716d8f7ea4
masmix1 <4masmix1@gmail.com>
2017-08-11 16:40:32 +0200

    Add contains method in AdSources enum

--- core/src/com/gpteam/shopmanager/advertising/AdSources.java
+++ core/src/com/gpteam/shopmanager/advertising/AdSources.java
@@ -1,5 +1,13 @@
 package com.gpteam.shopmanager.advertising;
 
 public enum AdSources {
-    NEWSPAPER, RADIO, TV
+    NEWSPAPER, RADIO, TV;
+
+    public static boolean contains(AdSources adSource) {
+        for (AdSources x : AdSources.values()) {
+            if (x.name().contains(adSource.name()))
+                return true;
+        }
+        return  false;
+    }
 }
