6668561d3f2b4997a47e4b56ca82207160956051
masmix1 <4masmix1@gmail.com>
2017-08-11 05:03:50 +0200

    Remove unused code, add super constructor

--- core/src/com/gpteam/shopmanager/advertising/sources/newspaper/Newspaper.java
+++ core/src/com/gpteam/shopmanager/advertising/sources/newspaper/Newspaper.java
@@ -4,13 +4,7 @@ import com.gpteam.shopmanager.advertising.sources.AdSource;
 import com.gpteam.shopmanager.society.SocietyClass;
 
 public class Newspaper extends AdSource {
-    public void increaseFunds() {
-    }
-
-    public int getSocietyClassEffectivnes(SocietyClass societyClass) {
-        return 0;
-    }
-
-    public void getAdEffectivnes() {
+    public Newspaper(String funds, String MIN_FUNDS, String MAX_FUNDS) {
+        super(funds, MIN_FUNDS, MAX_FUNDS);
     }
 }
