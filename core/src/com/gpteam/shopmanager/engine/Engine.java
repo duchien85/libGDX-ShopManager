c08c8c7242095ca7a3fbca6139256849b54da019
masmix1 <4masmix1@gmail.com>
2017-08-15 02:57:02 +0200

    Remove anything related to module storing, invoke method

--- core/src/com/gpteam/shopmanager/engine/Engine.java
+++ core/src/com/gpteam/shopmanager/engine/Engine.java
@@ -2,6 +2,7 @@ package com.gpteam.shopmanager.engine;
 
 import com.badlogic.gdx.Gdx;
 import com.gpteam.shopmanager.economy.Economy;
+import com.gpteam.shopmanager.engine.interfaces.Module;
 import com.gpteam.shopmanager.engine.modules.error_handler.ErrorHandler;
 import com.gpteam.shopmanager.engine.modules.game_date.GameDate;
 import com.gpteam.shopmanager.time.Time;
@@ -12,7 +13,6 @@ import java.util.HashMap;
  * Created by masmix on 07.02.2017.
  */
 public final class Engine {
-    private HashMap<String,Class> modules;
     private Economy economy;
     private GameDate gameDate;
     private Time time;
@@ -26,26 +26,15 @@ public final class Engine {
     public static int[] citizenSocietyClasses;
 
     public void initialize() {
-        modules = new HashMap<String, Class>();
-        modules.put("ErrorHandler", ErrorHandler.class);
+
 
         economy = new Economy();
         gameDate = new GameDate();
         time = new Time();
     }
 
-    @SuppressWarnings("unchecked")
-    public void invoke(String className, String methodName) {
-        try { modules.get(className).getDeclaredMethod(methodName).invoke(null); }
-        catch (Exception ex) { ErrorHandler.handleNoSuchMethodException("No such method. Found: " + methodName); }
-    }
-
     public void update() {
         economy.update();
-    }
-
-    public HashMap<String, Class> getModules() {
-        return modules;
     }
 
     public Economy getEconomy() {
