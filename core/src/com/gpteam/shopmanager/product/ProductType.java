c5da0fc01f3a18ab9109447b66361703b8fbe66c
masmix1 <4masmix1@gmail.com>
2017-08-16 21:05:42 +0200

    Rename ProductView class, to ProductType (and make it an enum)

---
+++ core/src/com/gpteam/shopmanager/product/ProductType.java
@@ -0,0 +1,14 @@
+package com.gpteam.shopmanager.product;
+
+import com.gpteam.shopmanager.engine.modules.error_handler.ErrorHandler;
+import com.gpteam.shopmanager.engine.modules.text_handler.Text;
+import com.gpteam.shopmanager.engine.modules.text_handler.TextHandler;
+
+/**
+ * Created by masmix on 14.05.2017.<p>
+ *
+ * Helper class that should be used when you create a new product.
+ */
+public enum ProductType {
+    APPLE
+}
