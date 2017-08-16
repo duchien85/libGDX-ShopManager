2604fe5876c3f8aa8ea62b1a19db64b32a894f4d
masmix1 <4masmix1@gmail.com>
2017-08-16 20:28:28 +0200

    Clarify TODO

--- core/src/com/gpteam/shopmanager/company/producer/Producer.java
+++ core/src/com/gpteam/shopmanager/company/producer/Producer.java
@@ -3,7 +3,7 @@ package com.gpteam.shopmanager.company.producer;
 import com.gpteam.shopmanager.company.Company;
 
 public class Producer extends Company {
-    // TODO certain production speed (product/sec or min) that may be improved (along with product quality)
+    // TODO every company has certain production speed (product/sec or min) that may be improved (along with product quality)
     private double productionSpeed;
 
     public Producer(String shortName, String fullName) {
