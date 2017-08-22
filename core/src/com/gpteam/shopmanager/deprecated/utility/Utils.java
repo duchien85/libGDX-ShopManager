3c33497e45076a2d78b6a21fb1ba06c89b92915f
masmix1 <4masmix1@gmail.com>
2017-08-22 20:51:14 +0200

    Move Utils to deprecated package

---
+++ core/src/com/gpteam/shopmanager/deprecated/utility/Utils.java
@@ -0,0 +1,4 @@
+package com.gpteam.shopmanager.deprecated.utility;
+
+public final class Utils {
+}
--- core/src/com/gpteam/shopmanager/engine/modules/utility/Utils.java
+++
@@ -1,6 +0,0 @@
-package com.gpteam.shopmanager.engine.modules.utility;
-
-import java.math.BigDecimal;
-
-public final class Utils {
-}
