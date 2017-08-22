0cba17ec9bd6ed2ca57eba77e2e48250e3df536e
masmix1 <4masmix1@gmail.com>
2017-08-22 18:29:12 +0200

    Remove both setScale methods and TODO

--- core/src/com/gpteam/shopmanager/engine/modules/utility/Utils.java
+++ core/src/com/gpteam/shopmanager/engine/modules/utility/Utils.java
@@ -3,18 +3,5 @@ package com.gpteam.shopmanager.engine.modules.utility;
 import java.math.BigDecimal;
 
 public final class Utils {
-    /** TODO Write wrapper class for BigDecimal, similiar to {@link com.gpteam.shopmanager.time.Date} wrapper class */
-    public static void setScale(BigDecimal bigDecimal, int scale) {
-        bigDecimal = bigDecimal.setScale(scale, BigDecimal.ROUND_HALF_UP);
-    }
-
-    /**
-     * Sets the scale of {@code bigDecimal} to default value of 2 and rounding mode to
-     * {@code ROUND_HALF_UP}.
-     *
-     * @param bigDecimal {@code Bigdecimal} object to have it's scale set
-     */
-    public static void setScale(BigDecimal bigDecimal) {
-        bigDecimal = bigDecimal.setScale(2, BigDecimal.ROUND_HALF_UP);
-    }
+    public static final int MONEY_SCALE = 2;
 }
