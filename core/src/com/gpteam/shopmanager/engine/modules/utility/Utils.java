f751a59aee9ae67918101262c8a2c971ef932450
masmix1 <4masmix1@gmail.com>
2017-08-22 17:28:47 +0200

    Add setScale method with one BigDecimal argument

--- core/src/com/gpteam/shopmanager/engine/modules/utility/Utils.java
+++ core/src/com/gpteam/shopmanager/engine/modules/utility/Utils.java
@@ -7,4 +7,14 @@ public final class Utils {
     public static void setScale(BigDecimal bigDecimal, int scale) {
         bigDecimal = bigDecimal.setScale(scale, BigDecimal.ROUND_HALF_UP);
     }
+
+    /**
+     * Sets the scale of {@code bigDecimal} to default value of 2 and rounding mode to
+     * {@code ROUND_HALF_UP}.
+     *
+     * @param bigDecimal {@code Bigdecimal} object to have it's scale set
+     */
+    public static void setScale(BigDecimal bigDecimal) {
+        bigDecimal = bigDecimal.setScale(2, BigDecimal.ROUND_HALF_UP);
+    }
 }
