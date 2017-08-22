cabcdaf539e719a76d0bd2f241689017a2ddb9b6
masmix1 <4masmix1@gmail.com>
2017-08-23 00:23:18 +0200

    Add Apple class

---
+++ core/src/com/gpteam/shopmanager/product/products/Apple.java
@@ -0,0 +1,21 @@
+package com.gpteam.shopmanager.product.products;
+
+import com.gpteam.shopmanager.product.Product;
+import com.gpteam.shopmanager.product.ProductCategory;
+import com.gpteam.shopmanager.product.ProductType;
+import com.gpteam.shopmanager.time.Date;
+
+public class Apple extends Product {
+    /**
+     * Remember to initialize the {@link com.gpteam.shopmanager.engine.modules.text_handler.TextHandler} class first!
+     *
+     * @param price          price of a single product (this field is converted to BigDecimal object,
+     *                       make sure to pass it this way: 2 places after comma eg. 1.23)
+     * @param quality        quality of the whole product quantity
+     * @param quantity       quantity of this product
+     * @param expirationDate format: YYYY.MM.DD
+     */
+    public Apple(String price, int quality, int quantity, Date expirationDate) {
+        super(price, quality, quantity, expirationDate, ProductType.APPLE, ProductCategory.FOOD);
+    }
+}
