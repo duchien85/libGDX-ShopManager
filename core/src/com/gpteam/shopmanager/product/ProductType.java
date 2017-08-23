34b9a75ff44ae1b9b091f596ff42dc3b4e1bbdf1
masmix1 <4masmix1@gmail.com>
2017-08-23 18:03:08 +0200

    Remove javadoc because it doesn't correspond to the class anymore

--- core/src/com/gpteam/shopmanager/product/ProductType.java
+++ core/src/com/gpteam/shopmanager/product/ProductType.java
@@ -4,10 +4,8 @@ import com.gpteam.shopmanager.engine.modules.error_handler.ErrorHandler;
 import com.gpteam.shopmanager.engine.modules.text_handler.Text;
 import com.gpteam.shopmanager.engine.modules.text_handler.TextHandler;
 
-/**
+/*
  * Created by masmix on 14.05.2017.
- *
- * <p>Helper class that should be used when you create a new product.
  */
 public enum ProductType {
     APPLE
