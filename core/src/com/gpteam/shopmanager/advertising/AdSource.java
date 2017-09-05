356d7b67468e284367204f0135f822225b1a06bd
Patro <patrykblaszczak1@gmail.com>
2017-09-05 20:20:12 +0100

    Modify AdSource javadoc

--- core/src/com/gpteam/shopmanager/advertising/AdSource.java
+++ core/src/com/gpteam/shopmanager/advertising/AdSource.java
@@ -10,9 +10,9 @@ import java.math.RoundingMode;
 import static com.gpteam.shopmanager.variables.Variables.MONEY_SCALE;
 
 /**
- * Each instance of this class is it's own ad source. Contains the operations such as increase to
- * the funds of add source {@link #increaseFunds}, etc. Make sure you add a corresponding entry to
- * the {@link AdSources} enum.
+ * Contains the operations such as increase to the funds of add source {@link #increaseFunds},
+ * etc. Make sure you add a corresponding entry to the {@link AdSources} enum. Each instance of
+ * this class is it's own ad source.
  *
  * <p> {@code Funds}, {@code amount} fields should always be passed as {@code String}, in integer
  * format.
