41bec0dcfc9010b7fdf1e5c46bd446cbcc38294b
Patro <patrykblaszczak1@gmail.com>
2017-09-05 20:40:45 +0100

    Add contains method in SocietyClass

--- core/src/com/gpteam/shopmanager/society/SocietyClass.java
+++ core/src/com/gpteam/shopmanager/society/SocietyClass.java
@@ -1,5 +1,7 @@
 package com.gpteam.shopmanager.society;
 
+import com.gpteam.shopmanager.engine.modules.error_handler.ErrorHandler;
+
 /*
  * Created by masmix on 18.01.2017.
  */
@@ -8,5 +10,16 @@ public enum SocietyClass {
 
     public static SocietyClass getRandom() {
         return values()[(int) (Math.random() * values().length)];
+    }
+
+    public static boolean contains(SocietyClass societyClass) {
+        if (societyClass == null)
+            ErrorHandler.handleNullPointerException("msg");
+
+        for (SocietyClass x : SocietyClass.values()) {
+            if (x.name().contains(societyClass.name()))
+                return true;
+        }
+        return  false;
     }
 }
